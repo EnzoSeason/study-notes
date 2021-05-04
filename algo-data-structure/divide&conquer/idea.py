236964c477a08c457cedd24cc0e1c65753d6e930
Jijie LIU <liujijieseason@gmail.com>
2021-05-04 11:15:56 +0200

    Create idea.py

---
+++ algo-data-structure/divide&conquer/idea.py
@@ -0,0 +1,63 @@
+from typing import Any
+
+
+MAX_LEVEL = 10
+
+
+def print_result():
+    pass
+
+
+def process(level, params):
+    pass
+
+
+def reverse_state(level):
+    pass
+
+
+def solve(params):
+    pass
+
+
+def split_problem(problem):
+    pass
+
+
+def merge_results(results):
+    pass
+
+
+def recursion(level, params) -> None:
+    # terminate
+    if level > MAX_LEVEL:
+        print_result()
+        return
+
+    # process the current level
+    process(level, params)
+
+    # move to next level
+    recursion(level + 1, params)
+
+    # Optional clean up
+    # reverse the state of current level
+    reverse_state(level)
+
+
+def divideAndConquer(problem, params) -> Any:
+    # terminate
+    if problem is None:
+        return solve(params)
+
+    # divide the current problem
+    subproblems = split_problem(problem)
+
+    # conquer the subproblems
+    results = []
+    for subproblem in subproblems:
+        result = divideAndConquer(subproblem, params)
+        results.append(result)
+
+    # process the results
+    merge_results(results)
