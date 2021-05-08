d3c419b23e21c1f06275995603419758a2f6f68c
Jijie LIU <liujijieseason@gmail.com>
2021-05-08 16:15:11 +0200

    Update word_search_1.py

--- algo-data-structure/trie/word_search_1.py
+++ algo-data-structure/trie/word_search_1.py
@@ -2,7 +2,13 @@ from typing import List
 
 
 class Solution:
-    def _dfs(self, board: List[List[str]], board_idx: List[int], word: str) -> bool:
+    def _dfs(
+        self,
+        board: List[List[str]],
+        visited: List[List[bool]],
+        board_idx: List[int],
+        word: str,
+    ) -> bool:
 
         # end conditions
         if len(word) == 0:
@@ -15,27 +21,34 @@ class Solution:
 
         if board[i][j] != word[0]:
             return False
+        
+        if visited[i][j]:
+            return False
 
         # prepare date
-        original_char = board[i][j]
-        board[i][j] = "#"
+        visited[i][j] = True
         # divide & conquer
-        top = self._dfs(board, [i - 1, j], word[1:])
-        bottom = self._dfs(board, [i + 1, j], word[1:])
-        left = self._dfs(board, [i, j - 1], word[1:])
-        right = self._dfs(board, [i, j + 1], word[1:])
+        top = self._dfs(board, visited, [i - 1, j], word[1:])
+        bottom = self._dfs(board, visited, [i + 1, j], word[1:])
+        left = self._dfs(board, visited, [i, j - 1], word[1:])
+        right = self._dfs(board, visited, [i, j + 1], word[1:])
         # recover data
-        board[i][j] = original_char
+        visited[i][j] = False
 
         return top or bottom or left or right
 
     def exist(self, board: List[List[str]], word: str) -> bool:
+        if len(board) == 0 or len(board[0]) == 0:
+            return False
         if len(word) == 0:
             return False
 
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if self._dfs(board, [i, j], word):
+        m, n = len(board), len(board[0])
+        visited = [[False] * n for _ in range(m)]
+
+        for i in range(m):
+            for j in range(n):
+                if self._dfs(board, visited, [i, j], word):
                     return True
 
         return False
