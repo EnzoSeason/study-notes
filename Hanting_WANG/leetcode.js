da3fcd1fd756f1fe2809a62b5a951f72fa1948db
wanghanting <hanting.wang@etu.unice.fr>
2021-05-13 16:33:59 +0800

    daily exercise

--- Hanting_WANG/leetcode.js
+++ Hanting_WANG/leetcode.js
@@ -1,6 +1,18 @@
-console.log('niubi')
-console.log('niubiniubi')
-console.log('niubi')
-console.log('niubiniubi')
-console.log('niubi')
-console.log('766767')
+// 数组中移除相关数字
+var removeElement = function(nums, val) {
+    let j = 0
+    for(let i=0; i<nums.length; i++){
+        if(nums[i]!= val){
+            nums[j]=nums[i]
+            j += 1
+        }
+    }
+    nums.length = j
+    return j
+  };
+  
+  nums = [3,2,2,3]
+  val = 3
+  var res = removeElement(nums, val)
+  console.log(nums)
+  console.log(res)
