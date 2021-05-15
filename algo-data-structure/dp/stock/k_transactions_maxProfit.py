9547b109ec07e2b4c9ff66319524e24ba49d1918
Jijie LIU <liujijieseason@gmail.com>
2021-05-15 22:48:39 +0200

    Update k_transactions_maxProfit.py

--- algo-data-structure/dp/stock/k_transactions_maxProfit.py
+++ algo-data-structure/dp/stock/k_transactions_maxProfit.py
@@ -4,28 +4,71 @@ from typing import List
 class Solution1:
     """
     https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/
+
+    DP state: dp[0:3][0:days]
+    ---
+    It represents the profit when we **sell** the share. It has 2 dimensions.
+
+    - First dimension is the **number of the transaction**, k
+
+      Only 3 states is needed.
+    
+      0: The states presents the profits without transaction, which is 0
+    
+      1, 2: The states represent the profits of the current number of the transaction and previous one alternately.
+    
+    - Second dimension is the day
+
+    DP State Compression
+    ---
+    We compress the dimension of **the number of transaction** so that DP state has 2 dimension instead of 3.
+
+
+    DP State Function
+    ----
+    We caches the states of day profit limited by the number of transaction.
+
+    It uses an external variable, buyMax, which keeps the max of profit when we buy a share.
+    We need to track dp states and this variable
+
+    - init
+      
+      buyMax = -prices[0]
+
+      profit = [[0 for _ in range(days)] for _ in range(3)]
+
+    - update profit when selling a share:
+        
+        profit[curr_trans][day] = max(
+            profit[curr_trans][day - 1], buyMax + prices[day]
+        )
+    
+    - update profit when buying a share:
+
+      buyMax = max(buyMax, profit[prev_trans][day - 1] - prices[day])
+    
+    - result
+
+      return max([trans[days - 1] for trans in profit])
+    
     """
 
     def maxProfit(self, k: int, prices: List[int]) -> int:
         days = len(prices)
         if days <= 1:
             return 0
-
-        # profit[0][0:days] means the max profit of each day without transaction,
-        # It's is an array filled with 0.
-        # profit[1][0:days] and profit[2][0:days] cache the max profit of each day,
-        # They represent the curr_transent transaction and prev_transious one alternately.
+        # init DP
         profit = [[0 for _ in range(days)] for _ in range(3)]
 
         for trans in range(1, k + 1):
             curr_trans, prev_trans = trans % 2 + 1, (trans - 1) % 2 + 1
             # Minimize the buyMax on the first day
-            # Buy the stock
+            # Buy the share
             buyMax = -prices[0]
             for day in range(1, days):
                 # profit[curr_trans][day] takes the max profit of
-                # holding the stock (profit[curr_trans][day - 1]) or
-                #  selling the stock (localMax + prices[day])
+                # holding the share (profit[curr_trans][day - 1]) or
+                # selling the share (buyMax + prices[day])
                 profit[curr_trans][day] = max(
                     profit[curr_trans][day - 1], buyMax + prices[day]
                 )
@@ -34,29 +77,62 @@ class Solution1:
                 # holding the stock or
                 # buying the stock (profit[prev_trans][day - 1] - prices[day])
                 buyMax = max(buyMax, profit[prev_trans][day - 1] - prices[day])
-        
+
         print(profit)
 
-        sells = [trans[days-1] for trans in profit]
-        return max(sells)
+        return max([trans[days - 1] for trans in profit])
 
 
 class Solution2:
     """
     https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/
 
-    DP state function is similar to the solution of two_transactions_maxProfit.
+    DP State: dp["sell", "buy"][0: k+1]
+    ---
+    It represents the profit when **selling and buying** the share.
+    - First dimension is stage of profit, selling or buying
+    - Second dimension is the number of transactions, k
+
+    DP State Compression
+    ---
+    We compress the dimension of the **days** so that DP state has 2 dimension instead of 3.
+
+    DP State Function
+    ---
+    We caches the states of transaction profit for each day.
+
+    - init
+
+      profit = [{"sell": 0, "buy": -prices[0]} for _ in range(k + 1)]
+
+    - update the profit when selling a share:
+      
+        profit[trans]["sell"] = max(
+            profit[trans]["sell"],
+            profit[trans]["buy"] + prices[day],
+        )
+    
+    - update the profit when buying a share
+
+        profit[trans]["buy"] = max(
+            profit[trans]["buy"],
+            profit[trans - 1]["sell"] - prices[day],
+        )
+    
+    - result:
+
+      return max([trans["sell"] for trans in profit])
     """
 
     def maxProfit(self, k: int, prices: List[int]) -> int:
         days = len(prices)
         if days <= 1:
             return 0
-        
-        profit = [{"sell": 0, "buy": float("-inf")} for _ in range(k + 1)]
+
+        profit = [{"sell": 0, "buy": -prices[0]} for _ in range(k + 1)]
 
         for day in range(days):
-            for trans in range(1, k+1):
+            for trans in range(1, k + 1):
                 profit[trans]["sell"] = max(
                     profit[trans]["sell"],
                     profit[trans]["buy"] + prices[day],
@@ -65,11 +141,10 @@ class Solution2:
                     profit[trans]["buy"],
                     profit[trans - 1]["sell"] - prices[day],
                 )
-        
+
         print(profit)
-        
-        sells = [trans["sell"] for trans in profit]
-        return max(sells)
+
+        return max([trans["sell"] for trans in profit])
 
 
 if __name__ == "__main__":
