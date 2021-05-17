08dce2e94717179b6ef38dd4309135ba9f11917a
Jijie LIU <liujijieseason@gmail.com>
2021-05-17 11:38:12 +0200

    Update cooldown_transactions_maxProfit.py

--- algo-data-structure/dp/stock/cooldown_transactions_maxProfit.py
+++ algo-data-structure/dp/stock/cooldown_transactions_maxProfit.py
@@ -2,11 +2,15 @@ from typing import List
 
 
 class Solution:
+    """
+    https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
+    """
+
     def maxProfit(self, prices: List[int]) -> int:
         days = len(prices)
         if days <= 1:
             return 0
-        
+
         sell, buy, cooldown = 0, -prices[0], 0
         for day in range(days):
             prev_sell = sell
@@ -14,12 +18,12 @@ class Solution:
             sell = max(sell, buy + prices[day])
             buy = max(buy, cooldown - prices[day])
             cooldown = max(cooldown, prev_sell)
-        
+
         return max(sell, cooldown)
 
 
 if __name__ == "__main__":
-    prices = [1,2,3,0,2]
+    prices = [1, 2, 3, 0, 2]
 
     s = Solution()
     print(s.maxProfit(prices))
