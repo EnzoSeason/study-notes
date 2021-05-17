a4279ce17c420870c36e700f563b78ff6ea65c75
Jijie LIU <liujijieseason@gmail.com>
2021-05-17 11:24:11 +0200

    Create cooldown_transactions_maxProfit.py

---
+++ algo-data-structure/dp/stock/cooldown_transactions_maxProfit.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        days = len(prices)
+        if days <= 1:
+            return 0
+        
+        sell, buy, cooldown = 0, -prices[0], 0
+        for day in range(days):
+            prev_sell = sell
+
+            sell = max(sell, buy + prices[day])
+            buy = max(buy, cooldown - prices[day])
+            cooldown = max(cooldown, prev_sell)
+        
+        return max(sell, cooldown)
+
+
+if __name__ == "__main__":
+    k = 2
+    prices = [1,2,3,0,2]
+
+    s = Solution()
+    print(s.maxProfit(prices))
