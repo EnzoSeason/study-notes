d3fb24bee87a32ae7fd98d647eaa1a9cf374c875
Jijie LIU <liujijieseason@gmail.com>
2021-05-18 11:09:42 +0200

    Update lengthOfLIS.py

--- algo-data-structure/dp/lengthOfLIS.py
+++ algo-data-structure/dp/lengthOfLIS.py
@@ -1,9 +1,11 @@
 from typing import List
 
 
-class Solution:
+class SolutionDP:
     """
     https://leetcode.com/problems/longest-increasing-subsequence/
+
+    using DP
     """
 
     def lengthOfLIS(self, nums: List[int]) -> int:
@@ -21,7 +23,55 @@ class Solution:
         return max(results)
 
 
+class Solution:
+    """
+    https://leetcode.com/problems/longest-increasing-subsequence/
+
+    maintain the LIS array using binary search
+    """
+
+    def __init__(self) -> None:
+        self.LIS = []
+
+    def updateLIS(self, target: int) -> None:
+        n = len(self.LIS)
+        if n == 0:
+            self.LIS.append(target)
+            return
+
+        low, high = 0, n - 1
+
+        while low <= high:
+            mid = low + (high - low) // 2
+            if target < self.LIS[mid]:
+                high = mid - 1
+            elif target > self.LIS[mid]:
+                low = mid + 1
+            else:
+                return
+
+        if low == n:
+            self.LIS.append(target)
+        else:
+            self.LIS[low] = target
+
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 0:
+            return 0
+
+        for num in nums:
+            self.updateLIS(num)
+
+        print(self.LIS)
+        return len(self.LIS)
+
+
 if __name__ == "__main__":
     nums = [10, 9, 2, 5, 3, 7, 101, 18]
+
+    s_dp = SolutionDP()
+    print(s_dp.lengthOfLIS(nums))
+
     s = Solution()
     print(s.lengthOfLIS(nums))
