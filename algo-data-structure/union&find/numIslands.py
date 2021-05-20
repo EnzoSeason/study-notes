a22caf7181c5ac3a9cb2bdfde167ef356abf935d
Jijie LIU <liujijieseason@gmail.com>
2021-05-20 16:11:52 +0200

    Create numIslands.py

---
+++ algo-data-structure/union&find/numIslands.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    """
+    https://leetcode.com/problems/number-of-islands/
+    """
+
+    def numIslands(self, grid: List[List[str]]) -> int:
+        pass
