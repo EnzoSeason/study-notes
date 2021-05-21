72a7dc12ee7b5e8bbbb965288788f35723a3c651
Jijie LIU <liujijieseason@gmail.com>
2021-05-21 16:32:27 +0200

    Update lfu.py

--- algo-data-structure/cache-replacement/lfu.py
+++ algo-data-structure/cache-replacement/lfu.py
@@ -26,3 +26,26 @@ class LFUCache:
         
 
     def put(self, key: int, value: int) -> None:
+        count = 0
+        p = self.head
+        last_node = self.head
+        
+        while p.next is not None:
+            if p.key == key:
+                p.freq += 1
+                p.val = value
+                self._remove(p)
+                self._insert(p)
+                return
+            else:
+                last_node = p
+                p = p.next
+                count += 1
+        
+        new_node = LinkedNode(key, value)
+        self._insert(new_node)
+        if count > self.capacity:
+            last_node.prev.next = None
+
+
+        
