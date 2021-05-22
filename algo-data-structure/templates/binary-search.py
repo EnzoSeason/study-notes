b75b34504ba28e000839c557687d51e1424cf243
Jijie LIU <liujijieseason@gmail.com>
2021-05-22 18:52:19 +0200

    Create binary-search.py

---
+++ algo-data-structure/templates/binary-search.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+def binarySearch(asc_array: List[int], target: int) -> bool:
+    """
+    Find an element in an asc sorted array.
+    """
+
+    left, right = 0, len(asc_array) - 1
+    while left <= right: # Attention: "<=" is used
+        mid = left + (right - left) // 2 # "//" is equivalent to floor() 
+        if asc_array[mid] == target:
+            return True
+        elif asc_array[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
