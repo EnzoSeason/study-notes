eac8ffe5d3251087a13431b49413fc14be20ea71
Jijie LIU <liujijieseason@gmail.com>
2021-05-22 18:36:39 +0200

    Update dfs.py

--- algo-data-structure/templates/dfs.py
+++ algo-data-structure/templates/dfs.py
@@ -14,13 +14,11 @@ class Solution:
         self.visited = set()
 
     def dfs(self, node: Node) -> None:
-        # 1. set the node visited
-        self.visited.add(node)
-        
-        # 2. process data according to the demand
+        # 1. process data according to the demand
         self.process(node)
+        self.visited.add(node)
 
-        # 3. do DFS on the not-visited children
+        # 2. do DFS on the not-visited children
         for child in node.children:
             if child not in self.visited:
                 self.dfs(child)
