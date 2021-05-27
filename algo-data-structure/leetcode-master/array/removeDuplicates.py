c28a9af8aac648c570aa38b5f63a590ec84131fe
Jijie LIU <liujijieseason@gmail.com>
2021-05-27 17:37:14 +0200

    Create removeDuplicates.py

---
+++ algo-data-structure/leetcode-master/array/removeDuplicates.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        fast, slow = 0, 0
+        while fast < len(nums) and slow < len(nums) - 1:
+            if nums[slow] != nums[fast]:
+                slow += 1
+                nums[slow] = nums[fast]
+            fast += 1
+        return slow
+
+
+if __name__ == "__main__":
+    nums = [1,1,2]
+
+    s = Solution()
+    print(s.removeDuplicates(nums))
