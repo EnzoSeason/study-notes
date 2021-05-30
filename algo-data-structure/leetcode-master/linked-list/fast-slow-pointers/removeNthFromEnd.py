36cffb137cf77a302de63369e8314ec4740ab91f
Jijie LIU <liujijieseason@gmail.com>
2021-05-30 15:36:43 +0200

    Create removeNthFromEnd.py

---
+++ algo-data-structure/leetcode-master/linked-list/fast-slow-pointers/removeNthFromEnd.py
@@ -0,0 +1,34 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    """
+    https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+    """
+
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        pivot = ListNode()
+        pivot.next = head
+
+        # init the interval between slow and fast
+        slow, fast = pivot, pivot
+        for _ in range(n):
+            if not fast:
+                return pivot.next
+            fast = fast.next
+        
+        # move the fast pointer to the tail
+        # remove the node pointed by slow
+        while fast.next:
+            slow = slow.next
+            fast = fast.next
+
+        if slow.next:
+            slow.next = slow.next.next
+        else:
+            slow.next = None
+
+        return pivot.next
