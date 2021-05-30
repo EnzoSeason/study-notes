6fd8b61b9556e292e3fa4c9bc72fb85b3a103701
Jijie LIU <liujijieseason@gmail.com>
2021-05-30 11:45:01 +0200

    Update designLinkedList.py

--- algo-data-structure/leetcode-master/linked-list/add-pivot/designLinkedList.py
+++ algo-data-structure/leetcode-master/linked-list/add-pivot/designLinkedList.py
@@ -14,24 +14,25 @@ class MyLinkedList:
         Initialize your data structure here.
         """
         self.pivot = MyNode()
+        self.size = 0
 
     def get(self, index: int) -> int:
         """
         Get the value of the index-th node in the linked list.
         If the index is invalid, return -1.
         """
+        if index < 0 or index >= self.size:
+            return -1
+
         curr = self.pivot.next
-        i = 0
-
-        while curr and i != index:
-            i += 1
-            curr = curr.next
-
-        return curr.val if curr else -1
+        for _ in range(index):
+            curr = curr.next            
+        
+        return curr.val
 
     def addAtHead(self, val: int) -> None:
         """
-        Add a node of value val before the first element of the linked list.
+        Add a node of value val before the first element of the linked list. 
         After the insertion, the new node will be the first node of the linked list.
         """
         node = MyNode(val)
@@ -40,49 +41,52 @@ class MyLinkedList:
         self.pivot.next = node
         node.next = head
 
+        self.size += 1
+
     def addAtTail(self, val: int) -> None:
         """
         Append a node of value val to the last element of the linked list.
         """
-        node = MyNode(val)
-
         p = self.pivot
         while p.next:
             p = p.next
-        p.next = node
+        p.next = MyNode(val)
+
+        self.size += 1
 
     def addAtIndex(self, index: int, val: int) -> None:
         """
-        Add a node of value val before the index-th node in the linked list.
-        If index equals to the length of linked list, the node will be appended to the end of linked list.
+        Add a node of value val before the index-th node in the linked list. 
+        If index equals to the length of linked list, the node will be appended to the end of linked list. 
         If index is greater than the length, the node will not be inserted.
         """
+        if index < 0 or index > self.size:
+            return
+
         node = MyNode(val)
         prev = self.pivot
-        i = 0
 
-        while prev.next and i != index:
+        for _ in range(index):
             prev = prev.next
-            i += 1
 
-        if prev:
-            if prev.next:
-                node.next = prev.next
-            prev.next = node
+        if prev.next:
+            node.next = prev.next
+        prev.next = node
+        self.size += 1
 
     def deleteAtIndex(self, index: int) -> None:
         """
         Delete the index-th node in the linked list, if the index is valid.
         """
+        if index < 0 or index >= self.size:
+            return
+
         prev = self.pivot
-        i = 0
-
-        while prev.next and i != index:
+        for _ in range(index):
             prev = prev.next
-            i += 1
 
-        if prev:
-            if prev.next:
-                prev.next = prev.next.next
-            else:
-                prev.next = None
+        if prev.next:
+            prev.next = prev.next.next
+        else:
+            prev.next = None
+        self.size -= 1
