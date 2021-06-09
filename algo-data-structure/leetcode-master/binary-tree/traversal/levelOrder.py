b57f9176ba7503545c70f584715825b7f29df5cd
Jijie LIU <liujijieseason@gmail.com>
2021-06-09 09:38:31 +0200

    Update levelOrder.py

--- algo-data-structure/leetcode-master/binary-tree/traversal/levelOrder.py
+++ algo-data-structure/leetcode-master/binary-tree/traversal/levelOrder.py
@@ -12,8 +12,12 @@ class Solution:
     """
     https://leetcode.com/problems/binary-tree-level-order-traversal/
     """
-    
+
     def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        """
+        BFS
+        """
+
         if not root:
             return []
 
@@ -33,19 +37,23 @@ class Solution:
             res.append(level)
 
         return res
-        
+
     def levelOrder_re(self, root: TreeNode) -> List[List[int]]:
+        """
+        DFS
+        """
+
         res = []
         self.level_worker(root, 1, res)
         return res
-    
+
     def level_worker(self, node: TreeNode, level: int, res: List[List[int]]) -> None:
         if node is None:
             return
         if len(res) < level:
             res.append([])
-        
-        res[level-1].append(node.val)
-        
-        self.level_worker(node.left, level+1, res)
-        self.level_worker(node.right, level+1, res)
+
+        res[level - 1].append(node.val)
+
+        self.level_worker(node.left, level + 1, res)
+        self.level_worker(node.right, level + 1, res)
