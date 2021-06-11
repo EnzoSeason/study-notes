44a4774a60194e79eaa31713876b55da2c34c82d
Jijie LIU <liujijieseason@gmail.com>
2021-06-11 20:45:44 +0200

    Update isBalanced.py

--- algo-data-structure/leetcode-master/binary-tree/backtracing/isBalanced.py
+++ algo-data-structure/leetcode-master/binary-tree/backtracing/isBalanced.py
@@ -10,11 +10,11 @@ class Solution:
     https://leetcode.com/problems/balanced-binary-tree/
     """
 
-    def getDepth(self, node: TreeNode) -> int:
+    def dfs(self, node: TreeNode) -> int:
         if not node:
             return 0
 
-        left, right = self.getDepth(node.left), self.getDepth(node.right)
+        left, right = self.dfs(node.left), self.dfs(node.right)
 
         if left == -1 or right == -1:
             return -1
@@ -22,4 +22,4 @@ class Solution:
         return 1 + max(left, right) if abs(left - right) <= 1 else -1
 
     def isBalanced(self, root: TreeNode) -> bool:
-        return self.getDepth(root) != -1
+        return self.dfs(root) != -1
