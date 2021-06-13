42d13af9732d503014f0b7deb1212b858e0aa55d
Jijie LIU <liujijieseason@gmail.com>
2021-06-13 21:52:59 +0200

    Update lowestCommonAncestor.py

--- algo-data-structure/leetcode-master/binary-tree/binary-search-tree/lowestCommonAncestor.py
+++ algo-data-structure/leetcode-master/binary-tree/binary-search-tree/lowestCommonAncestor.py
@@ -13,12 +13,16 @@ class Solution:
     def lowestCommonAncestor(
         self, root: TreeNode, p: TreeNode, q: TreeNode
     ) -> TreeNode:
+
+        if not root:
+            return
+
         if root.val < p.val and root.val < q.val:
             return self.lowestCommonAncestor(root.right, p, q)
-        
+
         if root.val > p.val and root.val > q.val:
             return self.lowestCommonAncestor(root.left, p, q)
-            
+
         # 1. root is None
         # 2. root is p or q
         # 3. root is between the p and q
