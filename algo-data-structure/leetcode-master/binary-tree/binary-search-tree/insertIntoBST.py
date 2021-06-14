98086c87ece3a4da2b36b2fcdd829bbad5ba0d29
Jijie LIU <liujijieseason@gmail.com>
2021-06-14 14:44:29 +0200

    Create insertIntoBST.py

---
+++ algo-data-structure/leetcode-master/binary-tree/binary-search-tree/insertIntoBST.py
@@ -0,0 +1,20 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    """
+    https://leetcode.com/problems/insert-into-a-binary-search-tree/
+    """
+
+    def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
+        if not root:
+            return TreeNode(val)
+        if root.val < val:
+            root.right = self.insertIntoBST(root.right, val)
+        if root.val > val:
+            root.left = self.insertIntoBST(root.left, val)
+        return root
