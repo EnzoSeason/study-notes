0950bb4df5b2f5260e135d6b55e8517215ff6c58
Jijie LIU <liujijieseason@gmail.com>
2021-06-15 10:57:51 +0200

    Create backtracking.py

---
+++ algo-data-structure/templates/backtracking.py
@@ -0,0 +1,25 @@
+class Solution:
+    """
+    Backtracking can be seen as the traversal of a graph.
+
+    It traverses horizontally and vertically.
+    """
+
+    def backtraking(self, params, res) -> None:
+        # The end condition of recursion.
+        # Collect the result.
+        if self.isEnd(params):
+            res.append(params)
+            return
+        
+        # The iteration here is the horizontal traversal.
+        for item in params:
+            new_item = self.process(item)
+            # The recursion here is the vertical traversal.
+            self.backtraking(new_item, res)
+
+    def isEnd(self, params) -> bool:
+        pass
+
+    def process(self, item) -> None:
+        pass
