1789b10ae6b8ae68759186ad6048bf8b2c93691e
Jijie LIU <liujijieseason@gmail.com>
2021-06-17 12:52:55 +0200

    Update subsetsWithDup.py

--- algo-data-structure/leetcode-master/backtracking/subsets/subsetsWithDup.py
+++ algo-data-structure/leetcode-master/backtracking/subsets/subsetsWithDup.py
@@ -8,8 +8,8 @@ class Solution:
     The way to remove the duplicates is similar to that of combinationSum2.
     https://leetcode.com/problems/combination-sum-ii/
 
-    
-    Nums aren't distinct. 
+
+    Nums aren't distinct.
     Therefore, we sort the nums and check self.nums[i] == self.nums[i - 1].
     """
 
@@ -27,6 +27,34 @@ class Solution:
 
             self.res.append(prev + [self.nums[i]])
             self.helper(prev + [self.nums[i]], i + 1)
+
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        self.nums = sorted(nums)
+        self.helper([], 0)
+        return self.res
+
+
+class Solution2:
+    """
+    https://leetcode.com/problems/subsets-ii/
+
+    Nums aren't distinct.
+    Therefore, we use visited set to track the visited items.
+    It's same as the previous solution, check self.nums[i] == self.nums[i - 1].
+    """
+
+    def __init__(self) -> None:
+        self.nums = []
+        self.res = [[]]
+
+    def helper(self, prev: List[int], start: int) -> None:
+
+        visited = set()
+        for i in range(start, len(self.nums)):
+            if self.nums[i] not in visited:
+                visited.add(self.nums[i])
+                self.res.append(prev + [self.nums[i]])
+                self.helper(prev + [self.nums[i]], i + 1)
 
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         self.nums = sorted(nums)
