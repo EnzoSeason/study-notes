44c27217ba77aac7b09ddb34ca71c1efea64c0f3
Jijie LIU <liujijieseason@gmail.com>
2021-06-18 11:25:45 +0200

    Create largestSumAfterKNegations.py

---
+++ algo-data-structure/leetcode-master/greedy/easy/largestSumAfterKNegations.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    """
+    https://leetcode.com/problems/maximize-sum-of-array-after-k-negations/
+    """
+
+    def largestSumAfterKNegations(self, nums: List[int], k: int) -> int:
+        #  sort nums by abs, desc
+        nums = sorted(nums, key=abs)[::-1]
+
+        # set neg num to pos
+        for i in range(len(nums)):
+            if nums[i] < 0 and k > 0:
+                nums[i] *= -1
+                k -= 1
+
+        # update the smallest abs num
+        if k % 2 == 1:
+            nums[-1] *= -1
+
+        return sum(nums)
