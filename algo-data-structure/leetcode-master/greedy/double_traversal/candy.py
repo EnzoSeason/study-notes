4752f2db6c8d3b6e6abe1a935a821ade95e50bdf
Jijie LIU <liujijieseason@gmail.com>
2021-06-22 16:33:31 +0200

    Create candy.py

---
+++ algo-data-structure/leetcode-master/greedy/double_traversal/candy.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    """
+    https://leetcode.com/problems/candy/
+    """
+
+    def candy(self, ratings: List[int]) -> int:
+        candies = [1] * len(ratings)
+
+        for i in range(1, len(ratings)):
+            if ratings[i] > ratings[i - 1]:
+                candies[i] = candies[i - 1] + 1
+
+        for i in range(len(ratings) - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                candies[i] = max(candies[i], candies[i + 1] + 1)
+
+        return sum(candies)
