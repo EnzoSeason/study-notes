f43d9e37fd95d41e9377a1e938900862305825a1
Jijie LIU <liujijieseason@gmail.com>
2021-06-22 17:10:02 +0200

    Create reconstructQueue.py

---
+++ algo-data-structure/leetcode-master/greedy/double_traversal/reconstructQueue.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    """
+    https://leetcode.com/problems/queue-reconstruction-by-height/
+    """
+
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        # sort by
+        # First item in DESC, then
+        # Second item in ASC
+        people = sorted(people, key=lambda x: (-x[0], x[1]))
+        
+        res = []
+        for p in people:
+            res.insert(p[1], p)
+        return res
