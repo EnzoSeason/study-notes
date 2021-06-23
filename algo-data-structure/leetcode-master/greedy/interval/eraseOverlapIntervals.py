205680d2d070e55563b9ecd181228b3668b50332
Jijie LIU <liujijieseason@gmail.com>
2021-06-23 11:17:12 +0200

    Update eraseOverlapIntervals.py

--- algo-data-structure/leetcode-master/greedy/interval/eraseOverlapIntervals.py
+++ algo-data-structure/leetcode-master/greedy/interval/eraseOverlapIntervals.py
@@ -7,14 +7,18 @@ class Solution:
     """
 
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        # sorted by upper bound in ASC
         intervals = sorted(intervals, key=lambda x: x[1])
-        count = 0
+        count = 0 # nb of removed intervals
         upper_bound = intervals[0][1]
 
         for i in range(1, len(intervals)):
             if intervals[i][0] < upper_bound:
+                # remove the interval
                 count += 1
             else:
+                # keep the interval
+                # update upper_bound
                 upper_bound = intervals[i][1]
 
         return count
