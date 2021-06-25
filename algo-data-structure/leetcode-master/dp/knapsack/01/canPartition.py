f0d0ae10eb864ce2774a89fd9f0b4c4abc4a0c9a
Jijie LIU <liujijieseason@gmail.com>
2021-06-25 17:50:48 +0200

    Update canPartition.py

--- algo-data-structure/leetcode-master/dp/knapsack/01/canPartition.py
+++ algo-data-structure/leetcode-master/dp/knapsack/01/canPartition.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-class Solution:
+class SolutionAccum:
     """
     https://leetcode.com/problems/partition-equal-subset-sum/
     """
@@ -18,3 +18,26 @@ class Solution:
                 dp[j] = max(dp[j], dp[j - num] + num)
 
         return dp[capacity] == capacity
+
+
+class Solution2:
+    """
+    https://leetcode.com/problems/partition-equal-subset-sum/
+    """
+
+    def canPartition(self, nums: List[int]) -> bool:
+        if sum(nums) % 2 == 1:
+            return False
+
+        capacity = sum(nums) // 2
+        dp = [0] * (capacity + 1)
+
+        if nums[0] <= capacity:
+            dp[nums[0]] = nums[0]
+
+        for i in range(1, len(nums)):
+            for j in range(capacity - nums[i], -1, -1):
+                if dp[j] != 0:
+                    dp[j + nums[i]] = max(dp[j + nums[i]], dp[j] + nums[i])
+
+        return max(dp) == capacity
