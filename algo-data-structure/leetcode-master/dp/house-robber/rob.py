53e0839791fc2f41a7dd94f059c117c2b09caf26
Jijie LIU <liujijieseason@gmail.com>
2021-06-29 11:44:56 +0200

    Update rob.py

--- algo-data-structure/leetcode-master/dp/house-robber/rob.py
+++ algo-data-structure/leetcode-master/dp/house-robber/rob.py
@@ -17,3 +17,21 @@ class Solution:
             dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
 
         return dp[-1]
+
+class Solution2:
+    """
+    https://leetcode.com/problems/house-robber/
+
+    dp table: use 2 variable instead of an array
+    """
+    
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        
+        prev, curr = nums[0], max(nums[0: 2])
+        
+        for i in range(2, len(nums)):
+            curr, prev = max(prev + nums[i], curr), curr
+        
+        return curr
