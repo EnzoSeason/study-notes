57da62e1e22fb40158503ace850f4d304d838767
Jijie LIU <liujijieseason@gmail.com>
2021-07-01 21:41:59 +0200

    Create longestCommonSubsequence.py

---
+++ algo-data-structure/leetcode-master/dp/subsequence/longestCommonSubsequence.py
@@ -0,0 +1,17 @@
+class Solution:
+    """
+    https://leetcode.com/problems/longest-common-subsequence/
+    """
+
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        n1, n2 = len(text1), len(text2)
+        dp = [[0 for _ in range(n2 + 1)] for _ in range(n1 + 1)]
+
+        for i in range(1, n1 + 1):
+            for j in range(1, n2 + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+        return dp[-1][-1]
