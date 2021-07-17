b2b614673fa2c6de6b1147ace8675ce168f0e64d
Jijie LIU <liujijieseason@gmail.com>
2021-07-17 20:30:13 +0200

    Update min-steps.py

--- algo-data-structure/amazon/min-steps.py
+++ algo-data-structure/amazon/min-steps.py
@@ -1,15 +1,15 @@
 from typing import List
 
 
-class Solution:
+class SolutionDFS:
     def __init__(self) -> None:
         self.dx = [-1, 1, 0, 0]
         self.dy = [0, 0, -1, 1]
         self.grid = []
         self.visited = set()
-        self.res = []
+        self.res = -1
 
-    def dfs(self, x: int, y: int, prev: list) -> None:
+    def dfs(self, x: int, y: int, prev: int) -> None:
         if x < 0 or x >= len(self.grid) or y < 0 or y >= len(self.grid[0]):
             return
 
@@ -17,8 +17,8 @@ class Solution:
             return
 
         if self.grid[x][y] == 9:
-            print(prev)
-            self.res.append(prev)
+            if self.res == -1 or self.res > prev:
+                self.res = prev
             return
 
         if self.grid[x][y] != 1:
@@ -26,24 +26,24 @@ class Solution:
 
         for i in range(4):
             self.visited.add((x, y))
-            self.dfs(x + self.dx[i], y + self.dy[i], prev + [(x, y)])
+            self.dfs(x + self.dx[i], y + self.dy[i], prev + 1)
             self.visited.remove((x, y))
 
     def minPath(self, grid: List[List[int]]) -> int:
         self.grid = grid
 
-        self.dfs(0, 0, [])
-        return min(self.res) if self.res else -1
+        self.dfs(0, 0, 0)
+        return self.res
 
 
 if __name__ == "__main__":
     grid = [
-        [1, 1, 1, 1, 1],
-        [1, 0, 0, 0, 1],
-        [1, 0, 1, 1, 1],
-        [1, 1, 1, 0, 1],
-        [0, 0, 0, 0, 9],
+        [1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 1, 0],
+        [1, 0, 1, 1, 1, 1],
+        [1, 1, 1, 0, 0, 1],
+        [0, 0, 0, 0, 9, 1],
     ]
 
-    s = Solution()
+    s = SolutionDFS()
     print(s.minPath(grid))
