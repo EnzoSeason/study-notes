fe2c1b870ebd317b32fd6277f569829383846d31
Jijie LIU <liujijieseason@gmail.com>
2021-07-21 15:57:42 +0200

    Update shortest-path-in-binary-matrix.py

--- algo-data-structure/amazon/shortest-path-in-binary-matrix.py
+++ algo-data-structure/amazon/shortest-path-in-binary-matrix.py
@@ -112,7 +112,78 @@ class SolutionBFS:
         return -1
 
 
+class SolutionDijkstra:
+    """
+    Dijkstra is based on BFS.
+
+    Instead of poplefting in a queue, poping the top of the minHeap.
+    minHeap is created according to the distance from the current node to the start.
+    """
+
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+
+        max_row = len(grid) - 1
+        max_col = len(grid[0]) - 1
+        directions = [
+            (-1, -1),
+            (-1, 0),
+            (-1, 1),
+            (0, -1),
+            (0, 1),
+            (1, -1),
+            (1, 0),
+            (1, 1),
+        ]
+
+        if grid[0][0] != 0 or grid[max_row][max_col] != 0:
+            return -1
+
+        queue = priority_queue = [(1, (0, 0))]
+        visited = set()
+        dist = 1
+
+        while queue:
+            distance, cell = heapq.heappop(priority_queue)
+
+            if cell in visited:
+                continue
+
+            if cell == (max_row, max_col):
+                return distance
+
+            visited.add(cell)
+
+            for row_difference, col_difference in directions:
+                new_row = cell[0] + row_difference
+                new_col = cell[1] + col_difference
+                if not (0 <= new_row <= max_row and 0 <= new_col <= max_col):
+                    continue
+                if grid[new_row][new_col] != 0:
+                    continue
+
+                if (new_row, new_col) in visited:
+                    continue
+                entry = (distance + 1, (new_row, new_col))
+                heapq.heappush(priority_queue, entry)
+
+        return -1
+
+
 class SolutionAStar:
+    """
+    A* is based on BFS.
+
+    Instead of poplefting in a queue, poping the top of the minHeap.
+    minHeap is created according to the "total" distance, d:
+
+    d = estimated_distance + current_distance
+
+    estimated_distance is the "direct" path from current node to the end, regardless the blocks.
+    Whether it's L1 or L2 distance depends on the context.
+
+    current_distance is the path from the current node to the start.
+    """
+
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         max_row = len(grid) - 1
         max_col = len(grid[0]) - 1
