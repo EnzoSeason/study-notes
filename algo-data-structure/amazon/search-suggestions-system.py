5f327e528a3fc811bbfe4995a4d36b3ba5e52c08
Jijie LIU <liujijieseason@gmail.com>
2021-07-23 15:50:26 +0200

    Update search-suggestions-system.py

--- algo-data-structure/amazon/search-suggestions-system.py
+++ algo-data-structure/amazon/search-suggestions-system.py
@@ -20,3 +20,37 @@ class SolutionBruteForce:
             res.append(sorted(suggestions)[:3])
 
         return res
+
+
+class SolutionBinarySearch:
+    def binarySearch_left(self, products: List[str], searchWord: str, lo: int) -> int:
+        l, r = lo, len(products) - 1
+        while l <= r:
+            mid = l + (r - l) // 2
+            if products[mid] == searchWord:
+                return mid
+            if products[mid] < searchWord:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return l
+
+    def suggestedProducts(
+        self, products: List[str], searchWord: str
+    ) -> List[List[str]]:
+        products.sort()
+
+        res = []
+        prefix = ""
+        lo = 0
+
+        for char in searchWord:
+            prefix += char
+            # lo = bisect.bisect_left(products, prefix, lo)
+            lo = self.binarySearch_left(products, prefix, lo)
+            suggestions = [
+                word for word in products[lo : lo + 3] if word.startswith(prefix)
+            ]
+            res.append(suggestions)
+
+        return res
