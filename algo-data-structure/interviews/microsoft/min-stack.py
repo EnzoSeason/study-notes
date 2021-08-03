b30fbf6151046461792210f4ecf8fa014ed90478
Jijie LIU <liujijieseason@gmail.com>
2021-08-03 11:54:57 +0200

    Create min-stack.py

---
+++ algo-data-structure/interviews/microsoft/min-stack.py
@@ -0,0 +1,31 @@
+class MinStack:
+    """
+    https://leetcode.com/problems/min-stack/
+    """
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.min_stack = []  # element is [val, nb of val]
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.min_stack or self.min_stack[-1][0] > val:
+            self.min_stack.append([val, 1])
+        elif self.min_stack[-1][0] == val:
+            self.min_stack[-1][1] += 1
+
+    def pop(self) -> None:
+        val = self.stack.pop()
+        if val == self.min_stack[-1][0]:
+            self.min_stack[-1][1] -= 1
+        if self.min_stack[-1][1] == 0:
+            self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1][0]
