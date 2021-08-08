ae0327e97a134980b005faf322fbff42fd2a2916
Jijie LIU <liujijieseason@gmail.com>
2021-08-08 21:57:39 +0200

    Update quick_sort.py

--- algo-data-structure/sort/quick_sort.py
+++ algo-data-structure/sort/quick_sort.py
@@ -9,42 +9,45 @@ def quick_sort(arr: List[int]):
     _quick_sort_worker(copied_arr, 0, n)
     return copied_arr
 
+
 def _quick_sort_worker(arr: List[int], start: int, end: int) -> None:
-    if start >= end - 1: 
+    if start >= end - 1:
         return
-    
+
     pivot_idx = _partition(arr, start, end)
-    print(arr, 'pivot is {} (index)'.format(pivot_idx))
+    print(arr, "pivot is {} (index)".format(pivot_idx))
     _quick_sort_worker(arr, start, pivot_idx)
-    _quick_sort_worker(arr, pivot_idx+1, end)
+    _quick_sort_worker(arr, pivot_idx + 1, end)
+
 
 def _partition(arr: List[int], start: int, end: int) -> int:
-    '''
+    """
     Tricky method:
 
-    1. choose a random item in the array, and put it at the first / last position
-    2. traverse the rest of array
-    '''
+    1. choose a random item in the array as the pivot.
+    2. split the array, all the items in arr[start: pivot_idx] < pivot, and  all the items in arr[pivot_idx + 1: end] > pivot
+    """
     k = random.randint(start, end - 1)
+    pivot = arr[k]
     arr[k], arr[end - 1] = arr[end - 1], arr[k]
     pivot_idx = start
 
     for i in range(start, end - 1):
-        if arr[i] > arr[end - 1]:
+        if arr[i] < pivot:
             arr[i], arr[pivot_idx] = arr[pivot_idx], arr[i]
             pivot_idx += 1
-        
+
     arr[end - 1], arr[pivot_idx] = arr[pivot_idx], arr[end - 1]
-    
+
     return pivot_idx
 
 
 if __name__ == "__main__":
     arr = [11, 8, 3, 9, 7, 1, 2, 5]
-    
-    print('Sort starts')
+
+    print("Sort starts")
     sorted_arr = quick_sort(arr)
-    print('Sort ends')
-    
-    print('orignal: ', arr)
-    print('sorted: ', sorted_arr)
+    print("Sort ends")
+
+    print("orignal: ", arr)
+    print("sorted: ", sorted_arr)
