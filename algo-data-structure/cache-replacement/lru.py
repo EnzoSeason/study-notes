3215d8cbaf37bdeea66c4c04bab10a483d27b607
Jijie LIU <liujijieseason@gmail.com>
2021-08-26 21:28:42 +0200

    Update lru.py

--- algo-data-structure/cache-replacement/lru.py
+++ algo-data-structure/cache-replacement/lru.py
@@ -1,5 +1,5 @@
-class LinkedNode:
-    def __init__(self, key, val):
+class DLinkedNode:
+    def __init__(self, key=None, val=None):
         self.key = key
         self.val = val
         self.next = None
@@ -8,60 +8,66 @@ class LinkedNode:
 
 class LRUCache:
     """
-    https://leetcode.com/problems/lru-cache/
-    https://en.wikipedia.org/wiki/Cache_replacement_policies#Least_recently_used_(LRU)
+    use 2 dummy nodes: head and tail
 
-    using doubly linked list. 
-    LinkedNode has both prev and next pointer 
+    + get: O(1)
+    + put: O(1)
     """
-
+    
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.head = LinkedNode(None, None)
-        self.count = 0
+        self.size = 0
+        self.cache = dict()
+        self.head, self.tail = DLinkedNode(), DLinkedNode()  #  dummy node
 
-    def _remove(self, node: LinkedNode) -> None:
-        prev_node, next_node = node.prev, node.next
-        prev_node.next = next_node
-        if next_node:
-            next_node.prev = prev_node
+        self.head.next, self.tail.prev = self.tail, self.head
 
-    def _moveToHead(self, node: LinkedNode) -> None:
-        first_node = self.head.next
+    def _add(self, node: DLinkedNode) -> None:
+        """
+        add the node to the head
+        """
+        first = self.head.next
+
         self.head.next = node
         node.prev = self.head
-        if first_node:
-            first_node.prev = node
-            node.next = first_node
+
+        node.next = first
+        first.prev = node
+
+    def _remove(self, node: DLinkedNode) -> DLinkedNode:
+        """
+        remove the node
+        """
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+        node.prev, node.next = None, None
+        return node
 
     def get(self, key: int) -> int:
-        p = self.head.next
-        while p is not None:
-            if p.key == key:
-                self._remove(p)
-                self._moveToHead(p)
-                return p.val
-            else:
-                p = p.next
-        return -1
+        node = self.cache.get(key, None)
+
+        if not node:
+            return -1
+
+        node = self._remove(node)
+        self._add(node)
+        return node.val
 
     def put(self, key: int, value: int) -> None:
-        p = self.head
-        last_node = self.head
-        count = 0
-        
-        while p is not None:
-            if p.key == key:
-                p.val = value
-                self._remove(p)
-                self._moveToHead(p)
-                return
-            else:
-                last_node = p
-                p = p.next
-                count += 1
+        node = self.cache.get(key, None)
 
-        new_node = LinkedNode(key, value)
-        self._moveToHead(new_node)
-        if count > self.capacity:
-            last_node.prev.next = None
+        if not node:
+            node = DLinkedNode(key, value)
+            self.cache[key] = node
+            self._add(node)
+            self.size += 1
+
+            if self.size > self.capacity:
+                tail = self._remove(self.tail.prev)
+                self.size -= 1
+                del self.cache[tail.key]
+        else:
+            node.val = value
+            node = self._remove(node)
+            self._add(node)
