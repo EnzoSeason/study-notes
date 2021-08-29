aaf29bbf9554e420192b0f392218bf7ad3c48ba0
Jijie LIU <liujijieseason@gmail.com>
2021-08-29 21:41:25 +0200

    Update counting_sort.py

--- algo-data-structure/sort/counting_sort.py
+++ algo-data-structure/sort/counting_sort.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 def _accumulate_bincount(nums: List[int]) -> List[int]:
     len_acc_bincounts = max(nums) + 1
     acc_bincounts = [0 for _ in range(len_acc_bincounts)]
@@ -9,22 +10,26 @@ def _accumulate_bincount(nums: List[int]) -> List[int]:
         acc_bincounts[num] += 1
     # accumulate
     for i in range(1, len_acc_bincounts):
-        acc_bincounts[i] += acc_bincounts[i-1]
-    
+        acc_bincounts[i] += acc_bincounts[i - 1]
+
     return acc_bincounts
 
-def _fill_sorted_nums(nums: List[int], acc_bincounts: List[int])-> List[int]:
-    sorted_nums = [0 for _ in nums]
-    i = 0
 
-    while i < len(nums):
+def _fill_sorted_nums(nums: List[int], acc_bincounts: List[int]) -> List[int]:
+    sorted_nums = [0 for _ in nums]
+    # The order is important.
+    # Sorting from bottom to top makes sort stable.
+    i = len(nums) - 1
+
+    while i >= 0:
         acc_count = acc_bincounts[nums[i]]
         sorted_nums[acc_count - 1] = nums[i]
 
         acc_bincounts[nums[i]] -= 1
-        i += 1
-    
+        i -= 1
+
     return sorted_nums
+
 
 def counting_sort(nums: List[int]) -> List[int]:
     acc_bincounts = _accumulate_bincount(nums)
@@ -32,10 +37,11 @@ def counting_sort(nums: List[int]) -> List[int]:
 
     return sorted_nums
 
+
 if __name__ == "__main__":
-    nums = [2,5,3,0,2,3,0,3]
+    nums = [2, 5, 3, 0, 2, 3, 0, 3]
 
     sorted_nums = counting_sort(nums)
-    
-    print('orignal: ', nums)
-    print('sorted: ', sorted_nums)
+
+    print("orignal: ", nums)
+    print("sorted: ", sorted_nums)
