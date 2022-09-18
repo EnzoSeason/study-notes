ee572a0f06a1a2e9bed836c262ebc2c310350a7e
Jijie LIU <liujijieseason@gmail.com>
2022-09-18 21:38:19 +0200

    Create 34-find-first-and-last-position-of-element-in-sorted-array.scala

---
+++ algo-data-structure/leetcode-master/scala/array/binary-search/34-find-first-and-last-position-of-element-in-sorted-array.scala
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+object Solution {
+  def searchRange(nums: Array[Int], target: Int): Array[Int] = {
+    val result = Array(-1, -1)
+    var left = 0
+    var right = nums.length
+
+    // search the left boundary
+    while (left < right) {
+      val mid = left + (right - left) / 2
+      if (nums(mid) < target) left = mid + 1
+      else right = mid
+    }
+
+    if (left == nums.length || nums(left) != target)
+      return Array(-1, -1)
+    result(0) = right
+
+    // search the right boundary
+    left = 0
+    right = nums.length
+    while (left < right) {
+      val mid = left + (right - left) / 2
+      if (nums(mid) > target) right = mid
+      else left = mid + 1
+    }
+
+    result(1) = left - 1
+    result
+  }
+}
