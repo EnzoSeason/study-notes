9fe6d9afe3e6a41651ff6311f1db0b34238c4c3b
Jijie LIU <liujijieseason@gmail.com>
2022-09-18 20:50:00 +0200

    Create 704-binary-search.scala

---
+++ algo-data-structure/leetcode-master/scala/array/binary-search/704-binary-search.scala
@@ -0,0 +1,13 @@
+object Solution {
+  def search(nums: Array[Int], target: Int): Int = {
+    var left = 0
+    var right = nums.length
+    while (left < right) {
+      val mid = left + (right - left) / 2
+      if (nums(mid) > target) right = mid
+      else if (nums(mid) < target) left = mid + 1
+      else return mid
+    }
+    -1
+  }
+}
