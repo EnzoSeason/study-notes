412b34dffd47428456cd745b30f9cb1b2b944287
Jijie LIU <liujijieseason@gmail.com>
2022-09-18 21:38:22 +0200

    Update Solution.scala

--- algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
+++ algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
@@ -1,15 +1,32 @@
 package playground
 
 object Solution {
-  def searchInsert(nums: Array[Int], target: Int): Int = {
+  def searchRange(nums: Array[Int], target: Int): Array[Int] = {
+    val result = Array(-1, -1)
     var left = 0
     var right = nums.length
+
+    // search the left boundary
     while (left < right) {
       val mid = left + (right - left) / 2
       if (nums(mid) < target) left = mid + 1
-      else if (nums(mid) > target) right = mid
-      else return mid
+      else right = mid
     }
-    left
+
+    if (left == nums.length || nums(left) != target)
+      return Array(-1, -1)
+    result(0) = right
+
+    // search the right boundary
+    left = 0
+    right = nums.length
+    while (left < right) {
+      val mid = left + (right - left) / 2
+      if (nums(mid) > target) right = mid
+      else left = mid + 1
+    }
+
+    result(1) = left - 1
+    result
   }
 }
