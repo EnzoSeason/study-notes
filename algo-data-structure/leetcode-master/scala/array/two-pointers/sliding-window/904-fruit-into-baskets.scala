1bfc189591ef6624e6fa7bb530a605c376c79c4b
Jijie LIU <liujijieseason@gmail.com>
2022-09-26 22:10:17 +0200

    Create 904-fruit-into-baskets.scala

---
+++ algo-data-structure/leetcode-master/scala/array/two-pointers/sliding-window/904-fruit-into-baskets.scala
@@ -0,0 +1,25 @@
+object Solution {
+  def totalFruit(fruits: Array[Int]): Int = {
+    val bag = collection.mutable.Map[Int, Int]()
+    var res = 0
+
+    var left = 0
+    var right = 0
+    while (right < fruits.length) {
+      bag(fruits(right)) = bag.getOrElse(fruits(right), 0) + 1
+
+      while (bag.size > 2) {
+        bag(fruits(left)) -= 1
+
+        if (bag(fruits(left)) == 0) bag -= fruits(left)
+
+        left += 1
+      }
+
+      res = math.max(right - left + 1, res)
+      right += 1
+    }
+
+    res
+  }
+}
