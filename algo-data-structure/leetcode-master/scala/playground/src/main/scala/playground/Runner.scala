880325e3e2b252dccc0b3e5611d02140acaf1e8d
Jijie LIU <liujijieseason@gmail.com>
2022-09-27 22:25:22 +0200

    Update Runner.scala

--- algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Runner.scala
+++ algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Runner.scala
@@ -3,5 +3,5 @@ package playground
 object Runner extends App {
   val solution = Solution
 
-  println(solution.totalFruit(Array(0,1,2,2)))
+  println(solution.spiralOrder(Array(Array(1, 2, 3), Array(4, 5, 6), Array(7, 8, 9))))
 }
