764061249506704a4b8011af94e40ab75bba45e2
Jijie LIU <liujijieseason@gmail.com>
2022-09-29 21:46:32 +0200

    Create 203-remove-linked-list-elements.scala

---
+++ algo-data-structure/leetcode-master/scala/linked-list/dummy-head/203-remove-linked-list-elements.scala
@@ -0,0 +1,24 @@
+// Definition for singly-linked list.
+class ListNode(_x: Int = 0, _next: ListNode = null) {
+  var next: ListNode = _next
+  var x: Int = _x
+}
+
+object Solution {
+  def removeElements(head: ListNode, target: Int): ListNode = {
+    val dummyHead = new ListNode(0, head)
+    var prev = dummyHead
+
+    while (prev.next != null) {
+      val curr = prev.next
+      if (curr.x == target) {
+        prev.next = curr.next
+        curr.next = null
+      } else {
+        prev = prev.next
+      }
+    }
+    
+    dummyHead.next
+  }
+}
