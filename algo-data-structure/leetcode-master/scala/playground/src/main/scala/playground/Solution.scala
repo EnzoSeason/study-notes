012f99e7555d7f73abaa1a6b8319822a23688078
Jijie LIU <liujijieseason@gmail.com>
2022-09-29 21:46:35 +0200

    Update Solution.scala

--- algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
+++ algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
@@ -1,34 +1,27 @@
 package playground
 
+
+// Definition for singly-linked list.
+class ListNode(_x: Int = 0, _next: ListNode = null) {
+  var next: ListNode = _next
+  var x: Int = _x
+}
+
 object Solution {
-  def spiralOrder(matrix: Array[Array[Int]]): List[Int] = {
-    val m = matrix.length
-    val n = matrix(0).length
-    val visited = scala.Array.ofDim[Boolean](m, n)
-    val dx = Array(0, 1, 0, -1)
-    val dy = Array(1, 0, -1, 0)
-    var res = List[Int]()
+  def removeElements(head: ListNode, target: Int): ListNode = {
+    val dummyHead = new ListNode(0, head)
+    var prev = dummyHead
 
-    var direction = 0
-    var i = 0
-    var j = 0
-    for (_ <- 0 until m * n) {
-      res = res :+ matrix(i)(j)
-      visited(i)(j) = true
-
-      val nextI = (i + dx(direction)) % m
-      val nextJ = (j + dy(direction)) % n
-      if (nextI < 0 ||
-        nextI >= m ||
-        nextJ < 0 ||
-        nextJ >= n ||
-        visited(nextI)(nextJ)) {
-        direction = (direction + 1) % 4
+    while (prev.next != null) {
+      val curr = prev.next
+      if (curr.x == target) {
+        prev.next = curr.next
+        curr.next = null
+      } else {
+        prev = prev.next
       }
-
-      i += dx(direction)
-      j += dy(direction)
     }
-    res
+
+    dummyHead.next
   }
 }
