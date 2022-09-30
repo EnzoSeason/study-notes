82f9ed151631fb1322fa651559280b53c40b230b
Jijie LIU <liujijieseason@gmail.com>
2022-09-30 21:41:06 +0200

    Update Solution.scala

--- algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
+++ algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
@@ -1,27 +1,89 @@
 package playground
 
-
-// Definition for singly-linked list.
-class ListNode(_x: Int = 0, _next: ListNode = null) {
-  var next: ListNode = _next
-  var x: Int = _x
+class SingleLinkedNode(_value: Int = -1, _next: SingleLinkedNode = null) {
+  var value: Int = _value
+  var next: SingleLinkedNode = _next
 }
+// Definition for singly-linked list.
+class MyLinkedList() {
 
-object Solution {
-  def removeElements(head: ListNode, target: Int): ListNode = {
-    val dummyHead = new ListNode(0, head)
-    var prev = dummyHead
+  val dummyHead: SingleLinkedNode = new SingleLinkedNode()
+  var size = 0
 
-    while (prev.next != null) {
-      val curr = prev.next
-      if (curr.x == target) {
-        prev.next = curr.next
-        curr.next = null
-      } else {
-        prev = prev.next
-      }
+  def get(index: Int): Int = {
+    if (index < 0 || index >= size) return -1
+
+    var i = 0
+    var p = dummyHead.next
+    while (p != null && i < index) {
+      p = p.next
+      i += 1
     }
 
-    dummyHead.next
+    if (p != null && i == index) p.value
+    else -1
   }
+
+  def addAtHead(value: Int): Unit = {
+    val newNode = new SingleLinkedNode(value)
+    val firstNode = dummyHead.next
+
+    dummyHead.next = newNode
+    newNode.next = firstNode
+    size += 1
+  }
+
+  def addAtTail(value: Int): Unit = {
+    val newNode = new SingleLinkedNode(value)
+
+    var prev = dummyHead
+    while (prev.next != null) {
+      prev = prev.next
+    }
+
+    prev.next = newNode
+    size += 1
+  }
+
+  def addAtIndex(index: Int, value: Int): Unit = {
+    if (index < 0 || index > size) return
+
+    val newNode = new SingleLinkedNode(value)
+
+    var prev = dummyHead
+    var curr = dummyHead.next
+    var i = 0
+
+    while (curr != null && i < index) {
+      curr = curr.next
+      prev = prev.next
+      i += 1
+    }
+
+    if (i == index) {
+      prev.next = newNode
+      newNode.next = curr
+      size += 1
+    }
+  }
+
+  def deleteAtIndex(index: Int): Unit = {
+    if (index < 0 || index >= size) return
+
+    var prev = dummyHead
+    var curr = dummyHead.next
+    var i = 0
+
+    while (curr != null && i < index) {
+      curr = curr.next
+      prev = prev.next
+      i += 1
+    }
+
+    if (i == index && curr != null) {
+      prev.next = curr.next
+      size -= 1
+    }
+  }
+
 }
