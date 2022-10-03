3ebd14036822ad782c8eb20f9544202ff4f062fd
Jijie LIU <liujijieseason@gmail.com>
2022-10-03 21:40:33 +0200

    Update Solution.scala

--- algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
+++ algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
@@ -7,15 +7,23 @@ class ListNode(_x: Int = 0, _next: ListNode = null) {
 }
 
 object Solution {
-  def swapPairs(head: ListNode): ListNode = {
-    if (head == null || head.next == null) return head
+  def removeNthFromEnd(head: ListNode, n: Int): ListNode = {
+    val dummyHead = new ListNode(-1, head)
+    var fast = dummyHead
+    var slow = dummyHead
 
-    val secondNode = head.next
-    val swappedPairs = swapPairs(secondNode.next)
+    for (_ <- 0 to n) {
+      if (fast == null) return dummyHead.next
+      fast = fast.next
+    }
 
-    secondNode.next = head
-    head.next = swappedPairs
+    while(fast.next != null) {
+      slow = slow.next
+      fast = fast.next
+    }
 
-    secondNode
+    val removedNode = slow.next
+    slow.next = if (removedNode != null) removedNode.next else null
+    head
   }
 }
