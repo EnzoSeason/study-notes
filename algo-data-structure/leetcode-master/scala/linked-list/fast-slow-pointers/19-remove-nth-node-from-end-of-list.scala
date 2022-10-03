0795c6464b27de1bdb95fc0df2bf6b1a218665d7
Jijie LIU <liujijieseason@gmail.com>
2022-10-03 21:40:35 +0200

    Create 19-remove-nth-node-from-end-of-list.scala

---
+++ algo-data-structure/leetcode-master/scala/linked-list/fast-slow-pointers/19-remove-nth-node-from-end-of-list.scala
@@ -0,0 +1,27 @@
+// Definition for singly-linked list.
+class ListNode(_x: Int = 0, _next: ListNode = null) {
+  var next: ListNode = _next
+  var x: Int = _x
+}
+
+object Solution {
+  def removeNthFromEnd(head: ListNode, n: Int): ListNode = {
+    val dummyHead = new ListNode(-1, head)
+    var fast = dummyHead
+    var slow = dummyHead
+
+    for (_ <- 0 to n) {
+      if (fast == null) return dummyHead.next
+      fast = fast.next
+    }
+
+    while(fast != null) {
+      slow = slow.next
+      fast = fast.next
+    }
+
+    val removedNode = slow.next
+    slow.next = if (removedNode != null) removedNode.next else null
+    head
+  }
+}
