ab49d8de1fa5adceb47df5ba716a71588ed06071
Jijie LIU <liujijieseason@gmail.com>
2022-10-05 21:38:10 +0200

    Create 142-linked-list-cycle-ii.scala

---
+++ algo-data-structure/leetcode-master/scala/linked-list/fast-slow-pointers/142-linked-list-cycle-ii.scala
@@ -0,0 +1,26 @@
+class ListNode(_x: Int = 0, _next: ListNode = null) {
+  var next: ListNode = _next
+  var x: Int = _x
+}
+
+object Solution {
+  def detectCycle(head: ListNode): ListNode = {
+    var fast = head
+    var slow = head
+
+    while (fast != null && fast.next != null) {
+      slow = slow.next
+      fast = fast.next.next
+
+      if (fast == slow) {
+        slow = head
+        while (slow != fast) {
+          slow = slow.next
+          fast = fast.next
+        }
+        return slow
+      }
+    }
+    null
+  }
+}
