4445c161d56dbf4d061f613fec8bdbf41302aa0c
Jijie LIU <liujijieseason@gmail.com>
2022-10-05 21:38:17 +0200

    Update Solution.scala

--- algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
+++ algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
@@ -7,23 +7,23 @@ class ListNode(_x: Int = 0, _next: ListNode = null) {
 }
 
 object Solution {
-  def removeNthFromEnd(head: ListNode, n: Int): ListNode = {
-    val dummyHead = new ListNode(-1, head)
-    var fast = dummyHead
-    var slow = dummyHead
+  def detectCycle(head: ListNode): ListNode = {
+    var fast = head
+    var slow = head
 
-    for (_ <- 0 to n) {
-      if (fast == null) return dummyHead.next
-      fast = fast.next
-    }
-
-    while(fast.next != null) {
+    while (fast != null && fast.next != null) {
       slow = slow.next
-      fast = fast.next
-    }
+      fast = fast.next.next
 
-    val removedNode = slow.next
-    slow.next = if (removedNode != null) removedNode.next else null
-    head
+      if (fast == slow) {
+        slow = head
+        while (slow != fast) {
+          slow = slow.next
+          fast = fast.next
+        }
+        return slow
+      }
+    }
+    null
   }
 }
