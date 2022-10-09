8237a4970e1dd6a94a30821641038230da507cfd
Jijie LIU <liujijieseason@gmail.com>
2022-10-09 21:33:32 +0200

    Update Solution.scala

--- algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
+++ algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
@@ -1,43 +1,15 @@
 package playground
 
 object Solution {
-  def threeSum(nums: Array[Int]): List[List[Int]] = {
-    scala.util.Sorting.quickSort(nums)
-    var result = List[List[Int]]()
+  def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+    var cache = Map[Int, Int]()
 
-    var i = 0
-    while (i < nums.length) {
-      if (nums(i) > 0) return result
-
-      while (i > 0 && i < nums.length && nums(i) == nums(i - 1)) {
-        i += 1
+    for (i <- 0 until nums.length) {
+      cache.get(target - nums(i)) match {
+        case Some(j) => return Array(j, i)
+        case _ => cache += (nums(i) -> i)
       }
-
-      var left = i + 1
-      var right = nums.length - 1
-      while (left < right) {
-        val localSum = nums(i) + nums(left) + nums(right)
-        if ( localSum == 0) {
-          result = (nums(i) :: nums(left) :: nums(right) :: Nil) :: result
-          left += 1
-          while (left < nums.length && nums(left) == nums(left - 1)){
-            left += 1
-          }
-          right -= 1
-          while (right > 0 && nums(right) == nums(right + 1)) {
-            right -= 1
-          }
-        }
-        else if (localSum < 0) {
-          left += 1
-        }
-        else {
-          right -= 1
-        }
-      }
-
-      i += 1
     }
-    result
+    Array[Int]()
   }
 }
