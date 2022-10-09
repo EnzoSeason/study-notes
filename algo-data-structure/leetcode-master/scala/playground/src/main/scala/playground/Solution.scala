04817ac36ab14d837ca3108f38df149691738d22
Jijie LIU <liujijieseason@gmail.com>
2022-10-09 20:38:32 +0200

    Update Solution.scala

--- algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
+++ algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
@@ -1,29 +1,43 @@
 package playground
 
-// Definition for singly-linked list.
-class ListNode(_x: Int = 0, _next: ListNode = null) {
-  var next: ListNode = _next
-  var x: Int = _x
-}
-
 object Solution {
-  def detectCycle(head: ListNode): ListNode = {
-    var fast = head
-    var slow = head
+  def threeSum(nums: Array[Int]): List[List[Int]] = {
+    scala.util.Sorting.quickSort(nums)
+    var result = List[List[Int]]()
 
-    while (fast != null && fast.next != null) {
-      slow = slow.next
-      fast = fast.next.next
+    var i = 0
+    while (i < nums.length) {
+      if (nums(i) > 0) return result
 
-      if (fast == slow) {
-        slow = head
-        while (slow != fast) {
-          slow = slow.next
-          fast = fast.next
-        }
-        return slow
+      while (i > 0 && i < nums.length && nums(i) == nums(i - 1)) {
+        i += 1
       }
+
+      var left = i + 1
+      var right = nums.length - 1
+      while (left < right) {
+        val localSum = nums(i) + nums(left) + nums(right)
+        if ( localSum == 0) {
+          result = (nums(i) :: nums(left) :: nums(right) :: Nil) :: result
+          left += 1
+          while (left < nums.length && nums(left) == nums(left - 1)){
+            left += 1
+          }
+          right -= 1
+          while (right > 0 && nums(right) == nums(right + 1)) {
+            right -= 1
+          }
+        }
+        else if (localSum < 0) {
+          left += 1
+        }
+        else {
+          right -= 1
+        }
+      }
+
+      i += 1
     }
-    null
+    result
   }
 }
