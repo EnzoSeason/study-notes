1de4b5ae2dbe9e879ec7ab93b731305f444b7294
Jijie LIU <liujijieseason@gmail.com>
2022-10-10 20:31:26 +0200

    Create 1-two-sum.scala

---
+++ algo-data-structure/leetcode-master/scala/array/two-pointers/n-sum/1-two-sum.scala
@@ -0,0 +1,13 @@
+object Solution {
+  def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+    var cache = Map[Int, Int]()
+
+    for (i <- 0 until nums.length) {
+      cache.get(target - nums(i)) match {
+        case Some(j) => return Array(j, i)
+        case _ => cache += (nums(i) -> i)
+      }
+    }
+    Array[Int]()
+  }
+}
