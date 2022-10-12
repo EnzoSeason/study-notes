a103a4b9dc0cbcbaf217cc12f00ed326966b615c
Jijie LIU <liujijieseason@gmail.com>
2022-10-12 20:59:52 +0200

    Update Solution.scala

--- algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
+++ algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
@@ -4,14 +4,22 @@ object Solution {
   def findAnagrams(s: String, p: String): List[Int] = {
     if (s.length < p.length) return List[Int]()
 
-    val target = p.toList.sorted
+    val target = Array.fill(26)(0)
+    val search = Array.fill(26)(0)
     var result = List[Int]()
 
-    var i = 0
-    while (i + target.length < s.length) {
-      val subList = s.slice(i, i + target.length).toList.sorted
-      if (subList == target) result = i :: result
-      i += 1
+    for (i <- 0 until p.length) {
+      search(s.charAt(i).toInt - 'a'.toInt) += 1
+      target(p.charAt(i).toInt - 'a'.toInt) += 1
+    }
+
+    for (i <- 0 until (s.length - p.length - 1)) {
+      if (search.sameElements(target)) result ::= i
+
+      if (i + p.length == s.length) return result.reverse
+
+      search(s.charAt(i).toInt - 'a'.toInt) -= 1
+      search(s.charAt(i + p.length) - 'a'.toInt) += 1
     }
 
     result.reverse
