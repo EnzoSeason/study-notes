a4973ac37bca7cac2d23ae29afd6342ad43a1a15
Jijie LIU <liujijieseason@gmail.com>
2022-10-16 18:17:26 +0200

    Update 1002-find-common-characters.scala

--- algo-data-structure/leetcode-master/scala/hash-map/1002-find-common-characters.scala
+++ algo-data-structure/leetcode-master/scala/hash-map/1002-find-common-characters.scala
@@ -1,4 +1,38 @@
-object Solution {
+object SolutionArrayCache {
+  def commonChars(words: Array[String]): List[String] = {
+    val globalCache = Array.fill(26)(0)
+    var result = List[String]()
+
+    // init cache
+    for (c <- words.head) {
+      globalCache(c.toInt - 'a'.toInt) += 1
+    }
+
+    // update cache
+    for (word <- words.tail) {
+      val localCache = Array.fill(26)(0)
+      for (c <- word) {
+        localCache(c.toInt - 'a'.toInt) += 1
+      }
+      for (i <- 0 until 26) {
+        globalCache(i) = math.min(globalCache(i), localCache(i))
+      }
+    }
+
+    // generate the result from the cache
+    for (i <- 0 until 26) {
+      val count = globalCache(i)
+      for (_ <- 0 until count) {
+        val charCode = i + 'a'.toInt
+        result :+= charCode.toChar.toString
+      }
+    }
+
+    result
+  }
+}
+
+object SolutionMapCache {
   def commonChars(words: Array[String]): List[String] = {
     val sortedWords = words.sortBy(_.length)
     var cache = Map[Char, Int]()
