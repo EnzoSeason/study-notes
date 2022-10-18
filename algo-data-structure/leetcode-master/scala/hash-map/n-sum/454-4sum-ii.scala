5bc3c6d353f8baa1932897359cd6a8e01f04ff19
Jijie LIU <liujijieseason@gmail.com>
2022-10-18 20:45:49 +0200

    scala: n sum problems

---
+++ algo-data-structure/leetcode-master/scala/hash-map/n-sum/454-4sum-ii.scala
@@ -0,0 +1,31 @@
+object Solution {
+  def fourSumCount(nums1: Array[Int], nums2: Array[Int], nums3: Array[Int], nums4: Array[Int]): Int = {
+    val n = nums1.length
+    var cache = Map[Int, Int]()
+    var count = 0
+
+    for {
+      i <- 0 until n
+      j <- 0 until n
+    } {
+      val localSum = nums1(i) + nums2(j)
+      cache.get(localSum) match {
+        case Some(value) => cache += (localSum -> (value + 1))
+        case None => cache += (localSum -> 1)
+      }
+    }
+
+    for {
+      k <- 0 until n
+      l <- 0 until n
+    } {
+      val localSum = - (nums3(k) + nums4(l))
+      cache.get(-localSum) match {
+        case Some(value) => count += value
+        case None => null
+      }
+    }
+
+    count
+  }
+}
