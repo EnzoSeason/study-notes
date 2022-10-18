c7d0cbc18319a76f531616b5696479a44f9087b3
Jijie LIU <liujijieseason@gmail.com>
2022-10-18 21:02:29 +0200

    Update Solution.scala

--- algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
+++ algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
@@ -3,33 +3,19 @@ package playground
 import scala.annotation.tailrec
 
 object Solution {
-  def fourSumCount(nums1: Array[Int], nums2: Array[Int], nums3: Array[Int], nums4: Array[Int]): Int = {
-    val n = nums1.length
-    var cache = Map[Int, Int]()
-    var count = 0
+  def canConstruct(ransomNote: String, magazine: String): Boolean = {
+    var cache = Array.fill(26)(0)
 
-    for {
-      i <- 0 until n
-      j <- 0 until n
-    } {
-      val localSum = nums1(i) + nums2(j)
-      cache.get(localSum) match {
-        case Some(value) => cache += (localSum -> (value + 1))
-        case None => cache += (localSum -> 1)
-      }
+    for (c <- magazine)  {
+      cache(c.toInt - 'a'.toInt) += 1
     }
 
-    for {
-      k <- 0 until n
-      l <- 0 until n
-    } {
-      val localSum = - (nums3(k) + nums4(l))
-      cache.get(-localSum) match {
-        case Some(value) => count += value
-        case None => null
-      }
+    for (c <- ransomNote) {
+      val i = c.toInt - 'a'.toInt
+      if (cache(i) == 0) return false
+      cache(i) -= 1
     }
 
-    count
+    true
   }
 }
