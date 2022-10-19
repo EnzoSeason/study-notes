6720a4736741b4d3ce7c955df3ba1ac3e49e8d15
Jijie LIU <liujijieseason@gmail.com>
2022-10-19 22:34:26 +0200

    Update 541-reverse-string-ii.scala

--- algo-data-structure/leetcode-master/scala/string/reverse/541-reverse-string-ii.scala
+++ algo-data-structure/leetcode-master/scala/string/reverse/541-reverse-string-ii.scala
@@ -1,6 +1,6 @@
 import scala.annotation.tailrec
 
-object Solution {
+object SolutionRec {
   def reverseStr(s: String, k: Int): String = {
     @tailrec
     def reverse(s: String, needToReverse: Boolean, history: String): String = {
@@ -9,5 +9,18 @@ object Solution {
       else reverse(s.drop(k), !needToReverse, history + subStr)
     }
     reverse(s, true, "")
+  }
+}
+
+object SolutionFunc {
+  def reverseStr(s: String, k: Int): String = {
+    // s = "abcdefg", k = 2
+    s.grouped(k) // Iterator ["ab", "cd", "ef", "g"]
+      .zipWithIndex // Iterator [("ab", 0), ("cd", 1), ("ef", 2), ("g", 3)]
+      .map {
+        case (subStr, index) => 
+          if (index % 2 == 0) subStr.reverse else subStr
+      }
+      .mkString
   }
 }
