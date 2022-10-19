4df05f52ef49d01a2845d2c0920c63e48fed8760
Jijie LIU <liujijieseason@gmail.com>
2022-10-19 22:34:14 +0200

    Update Solution.scala

--- algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
+++ algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
@@ -4,12 +4,11 @@ import scala.annotation.tailrec
 
 object Solution {
   def reverseStr(s: String, k: Int): String = {
-    @tailrec
-    def reverse(s: String, needToReverse: Boolean, history: String): String = {
-      val subStr = if (needToReverse) s.take(k).reverse else s.take(k)
-      if (s.length < k) history + subStr
-      else reverse(s.drop(k), !needToReverse, history + subStr)
-    }
-    reverse(s, true, "")
+    s.grouped(k)
+      .zipWithIndex
+      .map {
+        case (subStr, index) => if (index % 2 == 0) subStr.reverse else subStr
+      }
+      .mkString
   }
 }
