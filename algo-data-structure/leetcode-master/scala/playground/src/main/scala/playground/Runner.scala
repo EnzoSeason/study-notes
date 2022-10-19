d4b7de08c473ce4d073b81ad6c32bc4a59eaccea
Jijie LIU <liujijieseason@gmail.com>
2022-10-19 22:34:11 +0200

    Update Runner.scala

--- algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Runner.scala
+++ algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Runner.scala
@@ -3,5 +3,5 @@ package playground
 object Runner extends App {
   val solution = Solution
 
-  println(solution.commonChars(Array("bella","label","roller")))
+  println(solution.reverseStr("abcdefg", 2))
 }
