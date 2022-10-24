3cc385880b845ef73bbdd9d441c150683d4b7e77
Jijie LIU <liujijieseason@gmail.com>
2022-10-24 22:20:46 +0200

    Update Runner.scala

--- algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Runner.scala
+++ algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Runner.scala
@@ -3,5 +3,5 @@ package playground
 object Runner extends App {
   val solution = Solution
 
-  println(solution.strStr("aabaabaafa", "aabaaf"))
+  println(solution.repeatedSubstringPattern("aba"))
 }
