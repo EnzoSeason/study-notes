2ee1f4389b192254ab17be720c56165aee4ff502
Jijie LIU <liujijieseason@gmail.com>
2022-10-24 22:20:48 +0200

    Update Solution.scala

--- algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
+++ algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
@@ -3,43 +3,21 @@ package playground
 import scala.annotation.tailrec
 
 object Solution {
-  def strStr(haystack: String, needle: String): Int = {
-    val nextTable = getNextTable(needle)
+  def repeatedSubstringPattern(s: String): Boolean = {
+    val pmt = Array.fill(s.length)(0)
 
-    var i = 0
     var j = 0
-    while (i < haystack.length && j < needle.length) {
-      while (j >= 0 && haystack(i) != needle(j)) {
-        j = nextTable(j)
-      }
+    for (i <- 1 until s.length) {
+      while (j > 0 && s(i) != s(j)) j = pmt(j - 1)
 
-      j += 1
-      i += 1
+      if (s(i) != s(j)) pmt(i) = 0
+      else {
+        j += 1
+        pmt(i) = j
+      }
     }
 
-    if (j == needle.length) i - j
-    else -1
-  }
-
-  /**
-   * Create a Partial Match Table (PMT)
-   * for the word we need to find in the string
-   *
-   * In a word: NextTable = -1 :: PartialMatchTable
-   */
-  def getNextTable(word: String): Array[Int] = {
-    val nextTable = Array.fill(word.length + 1)(-1)
-
-    var j = -1
-    for (i <- 0 until word.length) {
-      while (j >= 0 && word(i) != word(j)) {
-        j = nextTable(j)
-      }
-
-      j += 1
-      nextTable(i + 1) = j
-    }
-
-    nextTable
+    val n = s.length
+    pmt.last != 0 && n % (n - pmt.last) == 0
   }
 }
