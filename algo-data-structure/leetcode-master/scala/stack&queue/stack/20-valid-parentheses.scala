609bac5324777ea67873db387a9286afaefe0135
Jijie LIU <liujijieseason@gmail.com>
2022-10-31 20:38:49 +0100

    Create 20-valid-parentheses.scala

---
+++ algo-data-structure/leetcode-master/scala/stack&queue/stack/20-valid-parentheses.scala
@@ -0,0 +1,23 @@
+object Solution {
+  def isValid(s: String): Boolean = {
+    val matchingTable = Map(
+      ')' -> '(',
+      ']' -> '[',
+      '}' -> '{'
+    )
+
+    val stack = scala.collection.mutable.Stack[Int]()
+
+    for (c <- s) {
+      matchingTable.get(c) match {
+        case Some(value) => {
+          if (stack.nonEmpty && stack.top == value) stack.pop()
+          else return false
+        }
+        case None => stack.push(c)
+      }
+    }
+
+    stack.isEmpty
+  }
+}
