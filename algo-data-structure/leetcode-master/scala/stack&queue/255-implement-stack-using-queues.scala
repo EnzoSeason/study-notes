66e49929a4f82e3e553df1b483deebd6d83964db
Jijie LIU <liujijieseason@gmail.com>
2022-10-31 20:11:33 +0100

    Create 255-implement-stack-using-queues.scala

---
+++ algo-data-structure/leetcode-master/scala/stack&queue/255-implement-stack-using-queues.scala
@@ -0,0 +1,27 @@
+class MyStack() {
+
+  private val _queue = scala.collection.mutable.Queue[Int]()
+
+  def push(x: Int): Unit = {
+    _queue.enqueue(x)
+  }
+
+  def pop(): Int = {
+    val n = _queue.length - 1
+
+    for (_ <- 0 until n) {
+      _queue.enqueue(_queue.dequeue())
+    }
+
+    _queue.dequeue()
+  }
+
+  def top(): Int = {
+    val result = pop()
+    _queue.enqueue(result)
+    result
+  }
+
+  def empty(): Boolean = _queue.isEmpty
+
+}
