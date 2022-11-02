b3d695da228c25154392374c08af7ee3588e46d9
Jijie LIU <liujijieseason@gmail.com>
2022-11-02 21:28:36 +0100

    Update Solution.scala

--- algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
+++ algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
@@ -1,16 +1,49 @@
 package playground
 
-import scala.annotation.tailrec
-
 object Solution {
-  def removeDuplicates(s: String): String = {
-    val stack = scala.collection.mutable.Stack[Char]()
+  def maxSlidingWindow(nums: Array[Int], k: Int): Array[Int] = {
+    val maxHeap = MaxHeap()
 
-    for (c <- s) {
-      if (stack.nonEmpty && stack.top == c) stack.pop()
-      else stack.push(c)
+    // init max heap
+    for (i <- 0 until k) {
+      maxHeap.add(nums(i))
     }
 
-    stack.mkString.reverse
+    var slow = 0
+    var fast = k
+    val result: Array[Int] = new Array[Int](nums.length - k + 1)
+    result(0) = maxHeap.top
+
+    while (fast < nums.length) {
+      maxHeap.remove(nums(slow))
+      maxHeap.add(nums(fast))
+
+      slow += 1
+      fast += 1
+      result(slow) = maxHeap.top
+    }
+
+    result.toArray
+  }
+
+  case class MaxHeap() {
+    import scala.collection.mutable
+
+    private var _queue = mutable.ArrayBuffer[Int]()
+
+    def add(elem: Int): Unit = {
+      while (_queue.nonEmpty && _queue.last < elem) {
+        _queue.remove(_queue.length - 1)
+      }
+      _queue.append(elem)
+    }
+
+    def remove(elem: Int): Unit = {
+      if (_queue.nonEmpty && _queue.head == elem) {
+        _queue.remove(0)
+      }
+    }
+
+    def top: Int = _queue.head
   }
 }
