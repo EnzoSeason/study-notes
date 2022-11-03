6006923b591dacd5521bb7aba030af51c75ae4aa
Jijie LIU <liujijieseason@gmail.com>
2022-11-03 12:28:45 +0100

    Update Runner.scala

--- algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Runner.scala
+++ algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Runner.scala
@@ -3,5 +3,5 @@ package playground
 object Runner extends App {
   val solution = Solution
 
-  println(solution.maxSlidingWindow(Array(9, 10, 9, -7, -4, -8, 2, -6), 5))
+  println(solution.topKFrequent(Array(1, 1, 1, 2, 2, 3), 2))
 }
