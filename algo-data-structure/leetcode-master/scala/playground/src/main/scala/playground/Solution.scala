2b3c9ab08d1dc1ad5a77093be94f5dd596b19027
Jijie LIU <liujijieseason@gmail.com>
2022-11-03 12:28:48 +0100

    Update Solution.scala

--- algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
+++ algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
@@ -1,50 +1,15 @@
 package playground
 
 object Solution {
-  def maxSlidingWindow(nums: Array[Int], k: Int): Array[Int] = {
-    val maxHeap = MaxHeap()
-
-    // init max heap
-    for (i <- 0 until k) {
-      maxHeap.add(nums(i))
-    }
-
-    var slow = 0
-    var fast = k
-    val result = new Array[Int](nums.length - k + 1)
-    result(0) = maxHeap.top
-
-    while (fast < nums.length) {
-      maxHeap.remove(nums(slow))
-      maxHeap.add(nums(fast))
-
-      slow += 1
-      fast += 1
-      result(slow) = maxHeap.top
-    }
-
-    result
-  }
-
-  case class MaxHeap() {
-
-    import scala.collection.mutable
-
-    private val _queue = mutable.ArrayBuffer[Int]()
-
-    def add(elem: Int): Unit = {
-      while (_queue.nonEmpty && _queue.last < elem) {
-        _queue.remove(_queue.length - 1)
+  def topKFrequent(nums: Array[Int], k: Int): Array[Int] = {
+    nums
+      .map((_, 1))
+      .groupBy(_._1)
+      .map {
+        case (num, freqMap) => (num, freqMap.map(_._2).sum)
       }
-      _queue.append(elem)
-    }
-
-    def remove(elem: Int): Unit = {
-      if (_queue.nonEmpty && _queue.head == elem) {
-        _queue.remove(0)
-      }
-    }
-
-    def top: Int = _queue.head
+      .toArray.sortBy(_._2).reverse
+      .take(k)
+      .map(_._1)
   }
 }
