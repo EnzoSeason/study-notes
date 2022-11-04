a90121fb325e1865c4787863c4b4ba86d568deb8
Jijie LIU <liujijieseason@gmail.com>
2022-11-04 11:40:53 +0100

    Create 145-binary-tree-postorder-traversal.scala

---
+++ algo-data-structure/leetcode-master/scala/binary-tree/traversal/145-binary-tree-postorder-traversal.scala
@@ -0,0 +1,34 @@
+class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
+  var value: Int = _value
+  var left: TreeNode = _left
+  var right: TreeNode = _right
+}
+
+object SolutionRec {
+    def postorderTraversal(root: TreeNode): List[Int] = {
+        if (root == null) Nil
+        else postorderTraversal(root.left) ::: postorderTraversal(root.right) ::: root.value :: Nil
+    }
+}
+
+object SolutionIter {
+
+  import scala.collection.mutable
+
+  def postorderTraversal(root: TreeNode): List[Int] = {
+    if (root == null) return Nil
+
+    val stack = mutable.Stack[TreeNode](root)
+    var result = List[Int]()
+
+    while (stack.nonEmpty) {
+      val node = stack.pop()
+      result :+= node.value
+
+      if (node.left != null) stack.push(node.left)
+      if (node.right != null) stack.push(node.right)
+    }
+
+    result.reverse
+  }
+}
