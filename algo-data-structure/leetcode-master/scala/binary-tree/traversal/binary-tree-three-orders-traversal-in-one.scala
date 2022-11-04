eae5c212d7cd9bb814f11590493cc68a93a8790b
Jijie LIU <liujijieseason@gmail.com>
2022-11-04 12:46:20 +0100

    Create binary-tree-three-orders-traversal-in-one.scala

---
+++ algo-data-structure/leetcode-master/scala/binary-tree/traversal/binary-tree-three-orders-traversal-in-one.scala
@@ -0,0 +1,59 @@
+object Solution {
+
+  import scala.collection.mutable
+
+  def traversal(root: TreeNode)(order: String): List[Int] = {
+    if (root == null) return Nil
+
+    val stack = mutable.Stack[TreeNode](root)
+    var result = List[Int]()
+
+    def preorder(): Unit = {
+      val node = stack.pop()
+
+      if (node.right != null) stack.push(node.right) // right
+      
+      if (node.left != null) stack.push(node.left) // left
+      
+      stack.push(node) // mid
+      stack.push(null) // mark as visited
+    }
+
+    def postorder(): Unit = {
+      val node = stack.pop()
+
+      stack.push(node) // mid
+      stack.push(null)
+      
+      if (node.right != null) stack.push(node.right) // right
+      
+      if (node.left != null) stack.push(node.left) // left
+    }
+
+    def inorder(): Unit = {
+      val node = stack.pop()
+
+      if (node.right != null) stack.push(node.right) // right
+      
+      stack.push(node) // mid
+      stack.push(null)
+      
+      if (node.left != null) stack.push(node.left) // left
+    }
+    
+    while (stack.nonEmpty) {
+      if (stack.top == null) {
+        stack.pop()
+        result :+= stack.pop()
+      } else {
+        order match {
+          case "preorder" => preorder()
+          case "postorder" => postorder()
+          case "inorder" => inorder()
+        }
+      }
+    }
+
+    result
+  }
+}
