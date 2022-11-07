9909c43a97675d566ea74ceea44144a0e15257f7
Jijie LIU <liujijieseason@gmail.com>
2022-11-07 22:51:33 +0100

    Update Runner.scala

--- algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Runner.scala
+++ algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Runner.scala
@@ -3,6 +3,7 @@ package playground
 object Runner extends App {
   val solution = Solution
 
-  val values = Array(1,null,2,3)
-  println(solution.traversal(Tree(values).build())("inorder"))
+  val values1: Array[Any] = Array(3,4,5,1,2,null,null,null,null,0)
+  val values2: Array[Any] = Array(4,1,2)
+  println(solution.isSubtree(Tree(values1).build(), Tree(values2).build()))
 }
