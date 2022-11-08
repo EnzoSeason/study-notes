7f1d853d7171bfb0cdba9e1c774de4b8e58d830f
Jijie LIU <liujijieseason@gmail.com>
2022-11-08 21:56:54 +0100

    Update Solution.scala

--- algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
+++ algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
@@ -5,23 +5,21 @@ object Solution {
 
   import scala.collection.mutable
 
-  def maxDepth(root: Node): Int = {
-    if (root == null) return 0
+  def isSymmetric(root: TreeNode): Boolean = {
+    if (root == null) return true
 
-    val queue = mutable.Queue[Node](root)
-    var depth = 0
-
-    while (queue.nonEmpty) {
-      val levelSize = queue.size
-      depth += 1
-      for (_ <- 0 until levelSize) {
-        val node = queue.dequeue()
-        for (child <- node.children) {
-          if (child != null) queue.enqueue(child)
-        }
+    val cache = mutable.Queue[(TreeNode, TreeNode)]((root.left, root.right))
+    while (cache.nonEmpty) {
+      cache.dequeue() match {
+        case (null, null) =>
+        case (_, null) => return false
+        case (null, _) => return false
+        case (left, right) =>
+          if (left.value != right.value) return false
+          cache.enqueue((left.left, right.right))
+          cache.enqueue((left.right, right.left))
       }
     }
-
-    depth
+    true
   }
 }
