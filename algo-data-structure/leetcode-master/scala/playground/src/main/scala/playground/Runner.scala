d4eb2a32e16204c63c4032d3942348bc7a3ba5f0
Jijie LIU <liujijieseason@gmail.com>
2022-11-11 18:09:56 +0100

    Update Runner.scala

--- algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Runner.scala
+++ algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Runner.scala
@@ -3,7 +3,6 @@ package playground
 object Runner extends App {
   val solution = Solution
 
-  val values1: Array[Any] = Array(3,4,5,1,2,null,null,null,null,0)
-  val values2: Array[Any] = Array(4,1,2)
-  println(solution.isSubtree(Tree(values1).build(), Tree(values2).build()))
+  val values1: Array[Any] = Array(1,2,3,null,5)
+  println(solution.binaryTreePaths(Tree(values1).build()))
 }
