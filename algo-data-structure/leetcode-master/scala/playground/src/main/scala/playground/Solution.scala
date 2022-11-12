2924cda6f4a9e6593482cebdab7a818db7cfba5d
Jijie LIU <liujijieseason@gmail.com>
2022-11-12 15:03:38 +0100

    Update Solution.scala

--- algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
+++ algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
@@ -4,29 +4,19 @@ object Solution {
 
   import scala.collection.mutable
 
-  def binaryTreePaths(root: TreeNode): List[String] = {
-    if (root == null) return List[String]()
+  def pathSum(root: TreeNode, targetSum: Int): List[List[Int]] = {
+    if (root == null) return Nil
 
-    val result = mutable.ListBuffer[String]()
-    val previousPaths = mutable.Stack[List[Int]](List())
-    val stack = mutable.Stack[TreeNode](root)
-
+    val result = mutable.ListBuffer[List[Int]]()
+    val stack = mutable.Stack[(TreeNode, List[Int])]((root, List[Int]()))
     while (stack.nonEmpty) {
-      val node = stack.pop()
-      val prevPath = previousPaths.pop()
-
-      if (node.left == null && node.right == null) {
-        result.append((prevPath :+ node.value).mkString("->"))
+      val (node, prev) = stack.pop()
+      if (node.left == null && node.right == null && node.value + prev.sum == targetSum) {
+        result.append(prev :+ node.value)
       }
 
-      if (node.right != null) {
-        stack.push(node.right)
-        previousPaths.push(prevPath :+ node.value)
-      }
-      if (node.left != null) {
-        stack.push(node.left)
-        previousPaths.push(prevPath :+ node.value)
-      }
+      if (node.right != null) stack.push((root.right, prev :+ node.value))
+      if (node.left != null) stack.push((root.left, prev :+ node.value))
     }
 
     result.toList
