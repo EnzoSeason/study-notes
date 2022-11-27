91b04d417c68a77cbdfa38649809708997a50dc0
Jijie LIU <liujijieseason@gmail.com>
2022-11-27 12:43:38 +0100

    Create 98-validate-binary-search-tree.scala

---
+++ algo-data-structure/leetcode-master/scala/binary-tree/binary-search-tree/98-validate-binary-search-tree.scala
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
+ *   var value: Int = _value
+ *   var left: TreeNode = _left
+ *   var right: TreeNode = _right
+ * }
+ */
+
+/**
+  * Preorder: Tracking the Min and Max during the recursion
+  */
+object Solution1 {
+  def isValidBST(root: TreeNode): Boolean = {
+    searchTree(root, Long.MinValue, Long.MaxValue)
+  }
+
+  def searchTree(node: TreeNode, minValue: Long, maxValue: Long): Boolean = {
+    if (node == null)
+      return true
+
+    if (node.value <= minValue || node.value >= maxValue)
+      return false
+
+    val leftCheck = searchTree(node.left, minValue, node.value)
+    val rightCheck = searchTree(node.right, node.value, maxValue)
+    leftCheck && rightCheck
+  }
+}
+
+/**
+  * Inorder: The list's order must be ASC.
+  */
+object Solution3 {
+
+  def isValidBST(root: TreeNode): Boolean = {
+    val nodes = inorderSearch(root)
+
+    if (nodes.length <= 1)
+      return true
+
+    for (i <- 1 until nodes.length) {
+      if (nodes(i - 1).value >= nodes(i).value)
+        return false
+    }
+
+    true
+  }
+
+  def inorderSearch(node: TreeNode): List[TreeNode] = {
+    if (node == null) Nil
+    else {
+      inorderSearch(node.left) ::: List(node) ::: inorderSearch(node.right)
+    }
+  }
+}
