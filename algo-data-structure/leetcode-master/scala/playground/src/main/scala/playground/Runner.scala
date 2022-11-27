1060be89fcdf820dde5ceaf9e31265d8235933b0
Jijie LIU <liujijieseason@gmail.com>
2022-11-27 12:43:44 +0100

    Update Runner.scala

--- algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Runner.scala
+++ algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Runner.scala
@@ -3,6 +3,6 @@ package playground
 object Runner extends App {
   val solution = Solution
 
-  val values1: Array[Any] = Array(1,2,3,null,5)
-  println(solution.binaryTreePaths(Tree(values1).build()))
+  val values1: Array[Any] = Array(0)
+  println(solution.isValidBST(Tree(values1).build()))
 }
