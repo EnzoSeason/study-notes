07d2f2c50038d574bd18dc0324a8b443c7965fd4
Jijie LIU <liujijieseason@gmail.com>
2022-11-28 21:02:20 +0100

    Update Solution.scala

--- algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
+++ algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
@@ -1,24 +1,24 @@
 package playground
 
 object Solution {
-  var minDiff = Int.MaxValue
-  var leftMax: TreeNode = null
-
   def getMinimumDifference(root: TreeNode): Int = {
-    search(root)
+    var minDiff = Int.MaxValue
+    var prevNode: TreeNode = null
+
+    def inorderSearch(node: TreeNode): Unit = {
+      if (node == null)
+        return
+
+      inorderSearch(node.left)
+
+      if (prevNode != null)
+        minDiff = Math.min(minDiff, Math.abs(node.value - prevNode.value))
+
+      prevNode = node
+      inorderSearch(node.right)
+    }
+
+    inorderSearch(root)
     minDiff
-  }
-
-  def search(node: TreeNode): Unit = {
-    if (node == null)
-      return
-
-    search(node.left)
-
-    if (leftMax != null)
-      minDiff = Math.min(minDiff, Math.abs(node.value - leftMax.value))
-    leftMax = node
-
-    search(node.right)
   }
 }
