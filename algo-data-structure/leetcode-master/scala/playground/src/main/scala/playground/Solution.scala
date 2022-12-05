5352c230c14f4f48e3394921670a11d76873135b
Jijie LIU <liujijieseason@gmail.com>
2022-12-05 20:16:27 +0100

    Update Solution.scala

--- algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
+++ algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
@@ -2,15 +2,16 @@ package playground
 
 object Solution {
   def lowestCommonAncestor(root: TreeNode, p: TreeNode, q: TreeNode): TreeNode = {
-    if (root == null || root == p || root == q) root
-    else {
-      val left = lowestCommonAncestor(root.left, p, q)
-      val right = lowestCommonAncestor(root.right, p, q)
-
-      if (left != null && right != null) root
-      else if (left != null && right == null) left
-      else if (left == null && right != null) right
-      else null
+    var node = root
+    while (node != null) {
+      if (node.value > p.value && node.value > q.value) {
+        node = node.left
+      } else if (node.value < p.value && node.value < q.value) {
+        node = node.right
+      } else {
+        return node
+      }
     }
+    null
   }
 }
