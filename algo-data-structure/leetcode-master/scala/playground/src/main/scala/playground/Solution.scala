aa0a99e7f6c53b69e6e482763177c48d09d12717
Jijie LIU <liujijieseason@gmail.com>
2022-12-06 22:10:02 +0100

    Update Solution.scala

--- algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
+++ algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
@@ -1,17 +1,39 @@
 package playground
 
 object Solution {
-  def lowestCommonAncestor(root: TreeNode, p: TreeNode, q: TreeNode): TreeNode = {
-    var node = root
-    while (node != null) {
-      if (node.value > p.value && node.value > q.value) {
-        node = node.left
-      } else if (node.value < p.value && node.value < q.value) {
-        node = node.right
-      } else {
-        return node
+  def deleteNode(root: TreeNode, key: Int): TreeNode = {
+    if (root == null) return null
+
+    if (root.value == key) {
+      if (root.left == null && root.right == null) {
+        // delete the current node (leaf)
+        return null
+      }
+      else if (root.left != null && root.right == null) {
+        // replace the current node by the left sub tree
+        return root.left
+      }
+      else if (root.left == null && root.right != null) {
+        // replace the current node by the right sub tree
+        return root.right
+      }
+      else {
+        var leaf = root.right
+        while (leaf.left != null) leaf = leaf.left
+        // set the leftest leaf as the current node
+        leaf.left = root.left
+        return root.right
       }
     }
-    null
+
+    if (root.value > key) {
+      root.left = deleteNode(root.left, key)
+    }
+
+    if (root.value < key) {
+      root.right = deleteNode(root.right, key)
+    }
+
+    root
   }
 }
