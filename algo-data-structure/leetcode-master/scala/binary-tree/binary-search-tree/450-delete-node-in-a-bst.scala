1819256e6d0f006e649a2b555790f705ca452774
Jijie LIU <liujijieseason@gmail.com>
2022-12-06 22:09:59 +0100

    Create 450-delete-node-in-a-bst.scala

---
+++ algo-data-structure/leetcode-master/scala/binary-tree/binary-search-tree/450-delete-node-in-a-bst.scala
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
+ *   var value: Int = _value
+ *   var left: TreeNode = _left
+ *   var right: TreeNode = _right
+ * }
+ */
+object Solution {
+  def deleteNode(root: TreeNode, key: Int): TreeNode = {
+    if (root == null) return null
+
+    if (root.value == key) {
+      if (root.left == null && root.right == null) {
+        // delete the current node (leaf)
+        return null
+      }
+      else if (root.left != null && root.right == null) {
+        // replace the current node by the left sub tree
+        return root.left
+      }
+      else if (root.left == null && root.right != null) {
+        // replace the current node by the right sub tree
+        return root.right
+      }
+      else {
+        var leaf = root.right
+        while (leaf.left != null) leaf = leaf.left
+        // set the leftest leaf as the current node
+        leaf.left = root.left
+        return root.right
+      }
+    }
+
+    if (root.value > key) {
+      root.left = deleteNode(root.left, key)
+    }
+
+    if (root.value < key) {
+      root.right = deleteNode(root.right, key)
+    }
+
+    root
+  }
+}
