ecb62e16ed64730614289f417231d41fe1cadab3
Jijie LIU <liujijieseason@gmail.com>
2022-12-07 21:07:31 +0100

    Update Solution.scala

--- algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
+++ algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
@@ -1,39 +1,21 @@
 package playground
 
 object Solution {
-  def deleteNode(root: TreeNode, key: Int): TreeNode = {
-    if (root == null) return null
+  def convertBST(root: TreeNode): TreeNode = {
+    var acc = 0
 
-    if (root.value == key) {
-      if (root.left == null && root.right == null) {
-        // delete the current node (leaf)
-        return null
-      }
-      else if (root.left != null && root.right == null) {
-        // replace the current node by the left sub tree
-        return root.left
-      }
-      else if (root.left == null && root.right != null) {
-        // replace the current node by the right sub tree
-        return root.right
-      }
-      else {
-        var leaf = root.right
-        while (leaf.left != null) leaf = leaf.left
-        // set the leftest leaf as the current node
-        leaf.left = root.left
-        return root.right
-      }
+    def sumNodeValue(node: TreeNode): Unit = {
+      if (node == null) return
+
+      sumNodeValue(node.right)
+
+      node.value += acc
+      acc = node.value
+
+      sumNodeValue(node.left)
     }
 
-    if (root.value > key) {
-      root.left = deleteNode(root.left, key)
-    }
-
-    if (root.value < key) {
-      root.right = deleteNode(root.right, key)
-    }
-
+    sumNodeValue(root)
     root
   }
 }
