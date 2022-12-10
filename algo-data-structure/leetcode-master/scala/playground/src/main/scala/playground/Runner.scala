6214091dcc85aed06c9909f216cd080e10862784
Jijie LIU <liujijieseason@gmail.com>
2022-12-10 11:20:26 +0100

    Update Runner.scala

--- algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Runner.scala
+++ algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Runner.scala
@@ -3,6 +3,5 @@ package playground
 object Runner extends App {
   val solution = Solution
 
-  val values1: Array[Any] = Array(1,null,5,3)
-  println(solution.getMinimumDifference(Tree(values1).build()))
+  println(solution.combine(4, 2))
 }
