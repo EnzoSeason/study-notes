727eb286e2d8f8f0a159edf818ec7ebd6916675a
Jijie LIU <liujijieseason@gmail.com>
2022-12-19 21:26:02 +0100

    refactor: combination to combinations

--- algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
+++ algo-data-structure/leetcode-master/scala/playground/src/main/scala/playground/Solution.scala
@@ -4,33 +4,19 @@ object Solution {
 
   import scala.collection.mutable
 
-  def restoreIpAddresses(s: String): List[String] = {
-    if (s.length < 4 || s.length > 12) return Nil
+  def subsets(nums: Array[Int]): List[List[Int]] = {
+    val result = mutable.Stack[List[Int]]()
+    val cache = mutable.Stack[Int]()
 
-    val result = mutable.Stack[String]()
-    val cache = mutable.Stack[String]()
-
-    def backtracking(startAt: Int): Unit = {
-      if (cache.length > 4) {
+    def backtracking(i: Int): Unit = {
+      if (i == nums.length) {
+        result.push(cache.reverse.toList)
         return
       }
-
-      if (startAt == s.length && cache.length == 4) {
-        result.push(cache.reverse.mkString("."))
-        return
-      }
-
-      for (endAt <- startAt until startAt + 3 if endAt < s.length && isValidNumber(startAt, endAt)) {
-        cache.push(s.substring(startAt, endAt + 1))
-        backtracking(endAt + 1)
-        cache.pop()
-      }
-    }
-
-    def isValidNumber(start: Int, end: Int): Boolean = {
-      if (start != end && s(start) == '0') return false
-      if (s.substring(start, end + 1).toInt > 255) false
-      else true
+      backtracking(i + 1)
+      cache.push(nums(i))
+      backtracking(i + 1)
+      cache.pop()
     }
 
     backtracking(0)
